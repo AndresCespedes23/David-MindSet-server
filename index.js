367c4d2f41de2a2ae0ab736fa3aa7fd0caa655fe
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-05 11:52:33 -0300

    This is a new branch

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const candidates = require('./controllers/candidates');
+const sessions = require('./controllers/sessions');
 
 const app = express();
 const port = 8000;
@@ -7,21 +7,21 @@ const port = 8000;
 app.get('/', (req, res) => {
     let html = '<h1>MindeSet</h1>';
     html += '<ul>';
-    html += '  <li>Candidates:';
+    html += '  <li>sessions:';
     html += '    <ul>';
-    html += '      <li><a href="/candidates">getAll</a></li>';
+    html += '      <li><a href="/sessions">getAll</a></li>';
     html += '    </ul>';
     html += '  </li>';
     html += '</ul>';
     res.send();
 });
 
-app.get('/candidates', candidates.getAll);
-app.get('/candidate/:id', candidates.getById);
-app.get('/candidate/byName/:name', candidates.getByName);
-app.get('/candidate/add', candidates.add);
-app.get('/candidate/edit', candidates.edit);
-app.get('/candidate/remove', candidates.remove);
+app.get('/sessions', sessions.getAll);
+app.get('/sessions/:id', sessions.getById);
+app.get('/sessions/byName/:name', sessions.getByName);
+app.get('/sessions/add', sessions.add);
+app.get('/sessions/edit', sessions.edit);
+app.get('/sessions/remove', sessions.remove);
 
 app.listen(port, () => {
   console.log(`Open your browser in http://localhost:${port}`);
