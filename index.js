4056ed5e426ccf74ce84ec8ce2eb04ed57a3a9b7
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-05 18:15:01 -0300

    copy format of the candidates example into the administrators resource files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const candidates = require('./controllers/candidates');
+const administrators = require('./controllers/administrators');
 
 const app = express();
 const port = 8000;
@@ -16,12 +17,31 @@ app.get('/', (req, res) => {
     res.send();
 });
 
+app.get('/', (req, res) => {
+  let html = '<h1>MindeSet</h1>';
+  html += '<ul>';
+  html += '  <li>Administrators:';
+  html += '    <ul>';
+  html += '      <li><a href="/administrators">getAll</a></li>';
+  html += '    </ul>';
+  html += '  </li>';
+  html += '</ul>';
+  res.send();
+});
+
 app.get('/candidates', candidates.getAll);
 app.get('/candidate/:id', candidates.getById);
 app.get('/candidate/byName/:name', candidates.getByName);
 app.get('/candidate/add', candidates.add);
 app.get('/candidate/edit', candidates.edit);
 app.get('/candidate/remove', candidates.remove);
+
+app.get('/administrators', administrators.getAll);
+app.get('/administrators/:id', administrators.getById);
+app.get('/administrators/byName/:name', administrators.getByName);
+app.get('/administrators/add', administrators.add);
+app.get('/administrators/edit', administrators.edit);
+app.get('/administrators/remove', administrators.remove);
 
 app.listen(port, () => {
   console.log(`Open your browser in http://localhost:${port}`);
