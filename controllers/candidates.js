9d33b861356edcccf20ca912fd0bf3823c0b5d5c
Valetín Feraudo <valentinferaudo@gmail.com>
2021-11-05 17:46:00 -0300

    Update getByID candidate

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -1,11 +1,18 @@
 const candidates = require('../data/candidates.json');
 
 const getAll = (req, res) => {
-    res.json(candidates)
+    res.json(candidates);
 };
 
 const getById = (req, res) => {
-    // your code here
+    const foundCandidate = candidates.list.some(candidate => candidate.id === parseInt(req.params.id));
+    if(!foundCandidate){
+        res.status(400).json({
+            ok:false,
+            msg:'Candidate not found'
+        });
+    }
+    res.json(candidates.list.filter(candidate => candidate.id === parseInt(req.params.id)))
 };
 
 const getByName = (req, res) => {
