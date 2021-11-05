01b7f5ea4f7d5f2e463d39e5811c3c0ae1d5a12b
User01 <user01@User01-3.local>
2021-11-05 10:10:20 -0300

    MS2021: Server sample

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const candidates = require('./controllers/candidates');
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    let html = '<h1>MindeSet</h1>';
+    html += '<ul>';
+    html += '  <li>Candidates:';
+    html += '    <ul>';
+    html += '      <li><a href="/candidates">getAll</a></li>';
+    html += '    </ul>';
+    html += '  </li>';
+    html += '</ul>';
+    res.send();
+});
+
+app.get('/candidates', candidates.getAll);
+app.get('/candidate/:id', candidates.getById);
+app.get('/candidate/byName/:name', candidates.getByName);
+app.get('/candidate/add', candidates.add);
+app.get('/candidate/edit', candidates.edit);
+app.get('/candidate/remove', candidates.remove);
+
+app.listen(port, () => {
+  console.log(`Open your browser in http://localhost:${port}`);
+});
