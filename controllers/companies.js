2bd3b29be3373b472a5126d72e2289210384abd4
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-06 12:35:34 -0300

    edit function complete

--- controllers/companies.js
+++ controllers/companies.js
@@ -2,14 +2,12 @@ const companies = require("../data/companies.json");
 
 const getAll = (req, res) => {
   res.json(companies);
-  console.log(req.protocol + "://" + req.get("host"));
-  //console.log(companies)
 };
 
 const getById = (req, res) => {
   const id = parseInt(req.params.id);
   const company = companies.find((company) => company.id === id);
-  if(company) {
+  if (company) {
     res.json(company);
   } else {
     res.status(404).json({ message: "Company not found" });
@@ -18,12 +16,12 @@ const getById = (req, res) => {
 
 const getByName = (req, res) => {
   const name = req.params.name;
-    const company = companies.find((company) => company.name === name);
-    if(company) {
-      res.json(company);
-    } else {
-      res.status(404).json({ message: "Company not found" });
-    }
+  const company = companies.find((company) => company.name === name);
+  if (company) {
+    res.json(company);
+  } else {
+    res.status(404).json({ message: "Company not found" });
+  }
 };
 
 const add = (req, res) => {
@@ -31,7 +29,16 @@ const add = (req, res) => {
 };
 
 const edit = (req, res) => {
-  // your code here
+  const id = parseInt(req.params.id);
+  const company = companies.find((company) => company.id === id);
+  if (company) {
+    const updateCompany = req.body
+    console.log(updateCompany)
+    company.name = updateCompany.name ? updateCompany.name : company.name;
+    res.json({ message: "Company updated successfully", company });
+  } else {
+    res.status(404).json({ message: "Company not found" });
+  }
 };
 
 const remove = (req, res) => {
