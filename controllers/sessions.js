777a9f152efa97454ffb04791472c560315be071
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-07 15:57:00 -0300

    getById func fn

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,12 +1,17 @@
 const sessionsData = require('../data/sessions.json');
 
-const getAll = (req, res) => {
-    res.json(sessionsData);
-};
+const getAll = (req, res) => res.json(sessionsData);
 
 const getById = (req, res) => {
-    // your code here
+    const found = sessionsData.some(session => session.id === parseInt(req.params.id));
+
+    if(found){
+        res.json(sessionsData.filter(session => session.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `No session with the id of ${req.params.id}`});
+    }
 };
+
 
 const getByName = (req, res) => {
     // your code here
