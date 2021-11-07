6a29558ca108640475e4fa2c37a644a1e5e9c7e2
Pablo Balbo <pablobalbo6@gmail.com>
2021-11-07 03:41:34 -0300

    Uncomment code lines

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const candidates = require('./controllers/candidates');
+const openPositions = require('./controllers/open-positions');
 
 const app = express();
 const port = 8000;
@@ -7,9 +8,9 @@ const port = 8000;
 app.get('/', (req, res) => {
     let html = '<h1>MindeSet</h1>';
     html += '<ul>';
-    html += '  <li>Candidates:';
+    html += '  <li>Open Positions:';
     html += '    <ul>';
-    html += '      <li><a href="/candidates">getAll</a></li>';
+    html += '      <li><a href="/open-positions">getAll</a></li>';
     html += '    </ul>';
     html += '  </li>';
     html += '</ul>';
@@ -22,6 +23,13 @@ app.get('/candidate/byName/:name', candidates.getByName);
 app.get('/candidate/add', candidates.add);
 app.get('/candidate/edit', candidates.edit);
 app.get('/candidate/remove', candidates.remove);
+
+app.get('/open-positions', openPositions.getAll);
+app.get('/open-positions/id/:id', openPositions.getById); 
+app.get('/open-positions/idCompany/:idCompany', openPositions.getByIdCompany);
+app.get('/open-positions/add', openPositions.add);
+app.get('/open-positions/edit', openPositions.edit);
+app.get('/open-positions/remove', openPositions.remove);
 
 app.listen(port, () => {
   console.log(`Open your browser in http://localhost:${port}`);
