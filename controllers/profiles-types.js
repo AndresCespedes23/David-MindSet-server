192434deb4d37a9f17cf93f506923190c370aeb0
AlfonsoScornavacca <alfondrums@gmail.com>
2021-11-07 04:13:05 -0300

    Function getById created

--- controllers/profiles-types.js
+++ controllers/profiles-types.js
@@ -1,4 +1,6 @@
-const profileTypes = require('../data/profiles-types.json')
+const fs = require('fs');
+const profileTypes = require('../data/profiles-types.json');
+
 const getAll = (req, res) => {
     // your code here
     res.json(profileTypes);
@@ -7,10 +9,17 @@ const getAll = (req, res) => {
 
 const getById = (req, res) => {
     // your code here
+    const profilesTypes  = profileTypes.find((profilesTypes) => profilesTypes.id === parseInt(req.params.id));
+    if (profilesTypes) {
+      res.json(profilesTypes);
+    } else {
+      res.status(404).json({ message: `Profile not found with id: ${parseInt(req.params.id)}` });
+    }
 };
 
 const getByName = (req, res) => {
     // your code here
+    
 };
 
 const add = (req, res) => {
