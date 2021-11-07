049c87faad5450f49bc1a6e386d7856fa6a86f66
Sofía Pagura <sofiapagura@gmail.com>
2021-11-07 18:48:00 -0300

    Create add fn

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -1,13 +1,47 @@
 const fs = require('fs');                                          // to step on json
 const interviews = require('../data/interviews.json');
-
+//git push origin ft-interviews-controller
 
 const getAll = (req, res) => {                                     // http://localhost:8000/interviews
     res.json(interviews);
 };
 
+// as David C.   - http://localhost:8000/interviews/add?id=215&idCompany=215&idCandidate=215&date=11/23/2021&status=true&isActive=true
+const add = (req, res) => { 
+    const newInterviews = {
+        id: req.query.id,
+        idCompany: req.query.idCompany,
+        idCandidate: req.query.idCandidate,
+        date: req.query.date,
+        status: req.query.status,
+        isActive: req.query.isActive
+    };
+    // if validation 
+    interviews.push(newInterviews);
+    fs.writeFile('./data/interviews.json', JSON.stringify(interviews), err => {
+        if (err) { res.send(500); }
+});
+    res.json(newInterviews);
+    console.log(req.query); // to check if i'm doing it rigth
 
-const getById = (req, res) => {                                // as by Traversy  - http://localhost:8000/interviews/150
+}
+
+
+
+/*
+NODE DVS 
+fs.writeFile('/Users/joe/test.txt', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+})
+*/
+
+
+
+const getById = (req, res) => {                   // as by Traversy  - http://localhost:8000/interviews/byId/150
     const findInterviewsId = interviews.some(interviews => interviews.id ===
         parseInt(req.params.id)); 
     if(findInterviewsId) {
@@ -18,16 +52,12 @@ const getById = (req, res) => {                                // as by Traversy
     
 };
 
-
 /*
 const getByName = (req, res) => {
     // your code here
 };
 */
 
-const add = (req, res) => {
-    // your code here
-};
 
 const edit = (req, res) => {
     // your code here
@@ -39,9 +69,9 @@ const remove = (req, res) => {
 
 module.exports = {
   getAll: getAll,
+  add: add,
   getById: getById,
   //getByName: getByName,
-  add: add,
   edit: edit,
   remove: remove
 };
--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.get('/candidate/remove', candidates.remove);
 
 //----INTERVIEWS----
 app.get('/interviews', interviews.getAll);
+app.get('/interviews/add', interviews.add);
 app.get('/interviews/:id', interviews.getById);
 // app.get('/interviews/byName/:name', interviews.getByName);
-app.get('/interviews/add', interviews.add);
 app.get('/interviews/edit', interviews.edit);
 app.get('/interviews/remove', interviews.remove);
 
