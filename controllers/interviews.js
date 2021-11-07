6329a29bc6b9204f3cc86b010564ba149f965e68
Sofía Pagura <sofiapagura@gmail.com>
2021-11-07 10:52:05 -0300

    Modified getById fn

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -1,16 +1,23 @@
-const fs = require('fs');   // to step on json
+const fs = require('fs');                                          // to step on json
 const interviews = require('../data/interviews.json');
 
 
-const getAll = (req, res) => {      // http://localhost:8000/interviews
+const getAll = (req, res) => {                                     // http://localhost:8000/interviews
     res.json(interviews);
 };
 
 
-const getById = (req, res) => {     // as by Traversy  - http://localhost:8000/interviews/150
-    res.json(interviews.filter(interviews => interviews.id === parseInt(req.params.id)))
+const getById = (req, res) => {                                // as by Traversy  - http://localhost:8000/interviews/150
+    const findInterviewsId = interviews.some(interviews => interviews.id ===
+        parseInt(req.params.id)); 
+    if(findInterviewsId) {
+    res.json(interviews.filter(interviews => interviews.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `Interview not found with the id of ${req.params.id}`});
+    }
     
 };
+
 
 /*
 const getByName = (req, res) => {
