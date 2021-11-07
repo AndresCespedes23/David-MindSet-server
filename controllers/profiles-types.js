c565d3718a182afa9d0e8720b6af46657cf936f1
AlfonsoScornavacca <alfondrums@gmail.com>
2021-11-07 09:56:04 -0300

    function ByName created

--- controllers/profiles-types.js
+++ controllers/profiles-types.js
@@ -9,9 +9,9 @@ const getAll = (req, res) => {
 
 const getById = (req, res) => {
     // your code here
-    const profilesTypes  = profileTypes.find((profilesTypes) => profilesTypes.id === parseInt(req.params.id));
-    if (profilesTypes) {
-      res.json(profilesTypes);
+    const profileTypesId  = profileTypes.find((profileTypesId) => profileTypesId.id === parseInt(req.params.id));
+    if (profileTypesId) {
+      res.json(profileTypesId);
     } else {
       res.status(404).json({ message: `Profile not found with id: ${parseInt(req.params.id)}` });
     }
@@ -19,6 +19,12 @@ const getById = (req, res) => {
 
 const getByName = (req, res) => {
     // your code here
+    const profileTypesName  = profileTypes.find((profileTypesName) => profileTypesName.name === (req.params.name));
+    if (profileTypesName) {
+      res.json(profileTypesName);
+    } else {
+      res.status(404).json({ message: `Profile not found with name: ${(req.params.name)}` });
+    }
     
 };
 
