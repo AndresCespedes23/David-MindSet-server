4d7f9c820811489d3f197c9f004b3e9764449060
Pablo Balbo <pablobalbo6@gmail.com>
2021-11-07 00:30:14 -0300

    Add new open position functionality added

--- controllers/open-positions.js
+++ controllers/open-positions.js
@@ -10,27 +10,46 @@ const getAll = (req, res) => {
     }
 };
 
-// const getById = (req, res) => {
-//     var filtererPositions = openPositions.filter(item => item.id.includes(req.param.id));
-//     res.json(filtererPositions);
-// };
+const add = (req, res) => {
+    // Add new open position
+    const newOpenPosition = {
+        id: openPositions.length + 1,
+        idCompany: req.query.idCompany,
+        startDate: req.query.startDate,
+        endDate: req.query.endDate,
+        jobDescription: req.query.jobDescription,
+        isActive: req.query.isActive
+    };
+    if(!newOpenPosition.id || !newOpenPosition.idCompany || !newOpenPosition.startDate || !newOpenPosition.endDate || !newOpenPosition.jobDescription || !newOpenPosition.isActive) {
+        res.send(400)
+    }
+    openPositions.push(newOpenPosition);
+    fs.writeFile('./data/open-positions.json', JSON.stringify(openPositions), err => {
+        if (err) {
+            res.send(500, 'Error trying to save new data');
+        }
+    });
+    res.json(newOpenPosition);
+};
 
 const getById = (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = parseInt(req.query.id);
     const openPosition = openPositions.find((openPosition) => openPosition.id === id);
     if (openPosition) {
       res.json(openPosition);
     } else {
       res.status(404).json({ message: `Open position not found with id: ${id}` });
     }
-  };
-
-const getByName = (req, res) => {
-    // your code here
 };
 
-const add = (req, res) => {
-    // your code here
+const getByIdCompany = (req, res) => {
+    const idCompany = parseInt(req.query.idCompany);
+    const openPositions = openPositions.find((openPosition) => openPosition.idCompany === idCompany);
+    if (openPositions) {
+      res.json(openPositions);
+    } else {
+      res.status(404).json({ message: `Open positions not found for company id: ${idCompany}` });
+    }
 };
 
 const edit = (req, res) => {
@@ -44,7 +63,7 @@ const remove = (req, res) => {
 module.exports = {
   getAll: getAll,
   getById: getById,
-  getByName: getByName,
+  getByIdCompany: getByIdCompany,
   add: add,
   edit: edit,
   remove: remove
