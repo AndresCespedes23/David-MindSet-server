15da0ab3197464b3e17a4c2e3f25e620669ce6fa
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-07 21:48:38 -0300

    edit func fn

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,39 +1,51 @@
-const sessionsData = require('../data/sessions.json');
+const fs = require('fs'); 
+const sessions = require('../data/sessions.json');
 
 const getAll = (req, res) => res.json(sessionsData);
 
 const getById = (req, res) => {
-    const found = sessionsData.some(session => session.id === parseInt(req.params.id));
+    const foundSessions = sessions.list.some(sessions => sessions.id === parseInt(req.params.id));
 
-    if(found){
-        res.json(sessionsData.filter(session => session.id === parseInt(req.params.id)));
+    if(foundSessions){
+        res.json(sessions.list.filter(sessions => sessions.id === parseInt(req.params.id)));
     } else {
         res.status(400).json({msg: `No session with the id of ${req.params.id}`});
     }
 };
 
-
 const getByName = (req, res) => {
     // your code here
 };
 
-const add = (req, res) => {
-    // your code here
-};
+const add = (req, res) => { 
+    let newSession = {
+        id: req.params.id,
+        idPsychologists: req.params.idPsychologists,
+        idCandidate: req.params.idCandidate,
+        date: req.params.date,
+        time: req.params.time,
+        isActive: req.params.isActive
+    }
+    sessions.list.push(newSession);
+    fs.writeFile('../data/sessions.json', JSON.stringify(sessions), err => {
+        if (err) { res.send(500) }
+    });
+    res.json(newSession);
+}
 
 const edit = (req, res) => {
     // your code here
 };
 
 const remove = (req, res) => {
-    // your code here
+     // your code here
 };
 
 module.exports = {
   getAll: getAll,
+  add: add,
   getById: getById,
   getByName: getByName,
-  add: add,
   edit: edit,
   remove: remove
 };
