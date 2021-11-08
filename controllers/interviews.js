da64009a2c1d4c121b28f0c90d67c6f087bc3b41
Sofía Pagura <sofiapagura@gmail.com>
2021-11-07 21:24:48 -0300

    Create remove fn

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -1,5 +1,5 @@
 const fs = require('fs');                // to step on json
-const interviews = require('../data/interviews.json');
+let interviews = require('../data/interviews.json');
 //git push origin ft-interviews-controller
 
 const getAll = (req, res) => {          // try:  http://localhost:8000/interviews
@@ -37,6 +37,7 @@ const getById = (req, res) => {                   // as by Traversy  - try: http
     }  
 };
 
+// test: http://localhost:8000/interviews/edit/1?idCandidate=4
 const edit = (req, res) => {
     const findInterviewsId = interviews.some(interviews => interviews.id === parseInt(req.params.id)); 
     const editInterviews = req.query;
@@ -59,40 +60,19 @@ const edit = (req, res) => {
     }
 };
 
-
-
-/*
-// follow fn update of Traversy and the fn add of David  - try: http://localhost:8000/interviews/edit
-// INTENTO FALLIDO Y DESARREGLADO 
-const edit = (req, res) => {        
-    const findInterviewsId = interviews.some(interviews => interviews.id === parseInt(req.query.id)); 
-    const editInterviews = req.query;
-    if(findInterviewsId) {
-        interviews.map(interviews => {
-            if(interviews.id ===parseInt(req.query.id)) {
-                interviews.idCompany = editInterviews.idCompany ? editInterviews.idCompany : interviews.idCompany;
-                interviews.idCandidate = editInterviews.idCandidate ? editInterviews.idCandidate : interviews.idCandidate;
-                interviews.date = editInterviews.date ? editInterviews.date : interviews.date;
-                interviews.status = editInterviews.status ? editInterviews.status : interviews.status;
-                interviews.isActive = editInterviews.isActive ? editInterviews.isActive : interviews.isActive;        
-            fs.writeFile('./data/interviews.json', JSON.stringify(interviews), err => {
-            if (err) { res.send(500); }
-        });
-        interviews.push(editInterviews);
-        res.json({ msg: 'Success! The edit of the interview was a success', newInterviews});
-    } else {
-        res.status(404).json({msg: `Interview not found with the id of ${req.query.id}`});
-    }
-};*/
-
-
-
-
-
-
-
 const remove = (req, res) => {
-    // your code here
+    const findInterviewsId = interviews.some(interviews => interviews.id === parseInt(req.params.id));
+    if(findInterviewsId) {
+        interviews = interviews.filter(interview => interview.id !== parseInt(req.params.id));
+        fs.writeFile('./data/interviews.json', JSON.stringify(interviews), (err) => {
+           if (err) {
+               res.status(500).json({ msg: 'Error removing Interview'});
+           }
+        });
+        res.json({ msg: 'Success: Interview removed'});
+    } else {
+        res.status(404).json({ msg: `Interview not found with the id of ${req.params.id}`});
+    } 
 };
 
 module.exports = {
