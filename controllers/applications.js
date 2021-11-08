5233ce4d1a9cf69dc0c3e1e641b87a9e503866d2
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-11-07 23:29:41 -0300

    added sorting for applications.json and edit functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,5 +1,5 @@
 let applications = require('../data/applications.json');
-
+applications.sort((a, b) => a.id - b.id);
 
 const getAll = (req, res) => {
     res.json(applications);
@@ -8,10 +8,10 @@ const getAll = (req, res) => {
 const getById = (req, res) => {
     const id = parseInt(req.params.id);
     const application = applications.find((applications) => applications.id === id);
-    if(application === undefined){
-        res.status(404).json({message : `no application with id: ${id}`});
+    if (application === undefined) {
+        res.status(404).json({ message: `no application with id: ${id}` });
     }
-    else{
+    else {
         res.json(application);
     }
 };
@@ -20,10 +20,10 @@ const getByIdPos = (req, res) => {
     const id = parseInt(req.params.id);
     console.log(id);
     const application = applications.filter((applications) => applications.idOpenPosition === id);
-    if(application === undefined){
-        res.status(404).json({message : `no applications with open position id: ${id}`});
+    if (application === undefined) {
+        res.status(404).json({ message: `no applications with open position id: ${id}` });
     }
-    else{
+    else {
         res.json(application);
     }
 };
@@ -32,10 +32,10 @@ const getByIdCan = (req, res) => {
     const id = parseInt(req.params.id);
     console.log(id);
     const application = applications.filter((applications) => applications.idCandidate === id);
-    if(application === undefined){
-        res.status(404).json({message : `no application with candidate id: ${id}`});
+    if (application === undefined) {
+        res.status(404).json({ message: `no application with candidate id: ${id}` });
     }
-    else{
+    else {
         res.json(application);
     }
 };
@@ -43,35 +43,54 @@ const getByIdCan = (req, res) => {
 const add = (req, res) => {
     let valid = req.query.idOpenPosition === undefined ? undefined : req.query.idCandidate === undefined ? undefined : true;
     let newId = applications[applications.length - 1].id + 1;
-    let newItem ={
-        id : newId,
-        idCandidate : req.query.idCandidate,
-        idOpenPosition : req.query.idOpenPosition,
-        isActive : true
+    let newItem = {
+        id: newId,
+        idCandidate: req.query.idCandidate,
+        idOpenPosition: req.query.idOpenPosition,
+        isActive: true
     }
-    if(valid){
+    if (valid) {
         applications.push(newItem);
-        console.log(newItem);
     }
-    else{
-        console.log("error");
+    else {
     }
+    res.json(newItem);
+    fs.writeFile('./data/applications.json', JSON.stringify(applications), error => {
+        if (error) { res.status(500) }
+    })
 };
 
 const edit = (req, res) => {
-    
+    const id = parseInt(req.query.id);
+    let editObj = applications.find((applications) => applications.id === id);
+    if (editObj) {
+        applications.map((editObj) => {
+            if (applications.id === id) {
+                editObj.idCandidate = req.query.idCandidate === undefined ? editObj.idCandidate : parseInt(req.query.idCandidate);
+                editObj.idOpenPosition = req.query.idOpenPosition === undefined ? editObj.idOpenPosition : parseInt(req.query.idOpenPosition);
+                editObj.isActive = req.query.isActive === undefined ? editObj.isActive : editObj.isActive == req.query.isActive ? editObj.isActive : !editObj.isActive;
+                return editObj;
+            }
+            return editObj;
+        })
+
+    }
+    res.json(editObj);
+    fs.writeFile('./data/applications.json', JSON.stringify(applications), error => {
+        if (error) { res.status(500) }
+    })
 };
 
 const remove = (req, res) => {
-    // your code here
+
 };
 
 module.exports = {
-  getAll: getAll,
-  getById: getById,
-  getByIdPos: getByIdPos,
-  getByIdCan: getByIdCan,
-  add: add,
-  edit: edit,
-  remove: remove
+    getAll: getAll,
+    getById: getById,
+    getByIdPos: getByIdPos,
+    getByIdCan: getByIdCan,
+    add: add,
+    edit: edit,
+    remove: remove
 };
