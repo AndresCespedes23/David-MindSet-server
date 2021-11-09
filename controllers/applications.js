8c4d22eb3775567b8bc77c9bc4ffb873c3a26a1f
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-11-08 23:31:54 -0300

    removed else from edit functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -70,33 +70,32 @@ const add = (req, res) => {
     }
     applications.push(newItem);
     fs.writeFile(path.join(__dirname, '../data/applications.json'), JSON.stringify(applications), (err) => {
-        if (error) { return res.status(500).json({message: `error adding application`})};
-        res.json({message: 'Application added'});
+        if (error) { return res.status(500).json({ message: `error adding application` }) };
+        res.json({ message: 'Application added' });
     })
 };
 
 const edit = (req, res) => {
     const id = parseInt(req.params.id);
     if (!id) {
-        res.status(400).json({ message: 'no id provided' }); //this line will never run
-    } else {
-        let editObj = applications.find((applications) => applications.id === id);
-        if (editObj == null) {
-            return res.status(404).json({ message: `no application with id: ${id}` });
-        }
-        applications.map((obj) => {
-            if (applications.id === id) {
-                editObj.idCandidate = editObj.idCandidate || parseInt(req.query.idCandidate);
-                editObj.idOpenPosition = editObj.idOpenPosition || parseInt(req.query.idOpenPosition);
-                editObj.isActive = editObj.isActive || req.query.isActive;
-                return editObj;
-            }
-        });
-        fs.writeFile(path.join(__dirname, '../data/applications.json'), JSON.stringify(applications), (err) => {
-            if (error) { return res.status(500).json({message: `error editing application`})};
-            res.json({message: 'Application added'});
-        });
+        return res.status(400).json({ message: 'no id provided' }); //this line will never run
     }
+    let editObj = applications.find((applications) => applications.id === id);
+    if (editObj == null) {
+        return res.status(404).json({ message: `no application with id: ${id}` });
+    }
+    applications.map((obj) => {
+        if (applications.id === id) {
+            editObj.idCandidate = editObj.idCandidate || parseInt(req.query.idCandidate);
+            editObj.idOpenPosition = editObj.idOpenPosition || parseInt(req.query.idOpenPosition);
+            editObj.isActive = editObj.isActive || req.query.isActive;
+            return editObj;
+        }
+    });
+    fs.writeFile(path.join(__dirname, '../data/applications.json'), JSON.stringify(applications), (err) => {
+        if (error) { return res.status(500).json({ message: `error editing application` }) };
+        res.json({ message: 'Application added' });
+    });
 };
 
 const remove = (req, res) => {
@@ -107,8 +106,8 @@ const remove = (req, res) => {
     }
     const newList = applications.filter((applications) => applications.id !== id);
     fs.writeFile(path.join(__dirname, '../data/applications.json'), JSON.stringify(applications), (err) => {
-        if (error) { return res.status(500).json({message: `error deleting application`})};
-        res.json({message: 'Application deleted'});
+        if (error) { return res.status(500).json({ message: `error deleting application` }) };
+        res.json({ message: 'Application deleted' });
     });
 }
 
