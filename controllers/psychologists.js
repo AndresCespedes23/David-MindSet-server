3cb657c328684a160953581afedeedd773864bec
Guido Glielmi <guidoglielmi@gmail.com>
2021-11-09 12:51:44 -0300

    Fixed inconsistencies

--- controllers/psychologists.js
+++ controllers/psychologists.js
@@ -3,141 +3,122 @@ const fs = require("fs");
 const path = require("path");
 
 const validate = (object) => {
-  for (let key in object) {
-    if (object[key] === undefined) {
-      return false;
-    }
-  }
-  return true;
+	for (let key in object) {
+		if (object[key] === undefined) {
+			return false;
+		}
+	}
+	return true;
 };
 
 const getLastId = (collection) => {
-  let larger = 0;
-  collection.forEach((element) => {
-    if (element.id > larger) {
-      larger = element.id;
-    }
-  });
-  return larger;
+	let larger = 0;
+	collection.forEach((element) => {
+		if (element.id > larger) {
+			larger = element.id;
+		}
+	});
+	return larger;
 };
 
 const getAll = (req, res) => {
-  res.json(psyList);
+	res.json(psyList);
 };
 
 const getById = (req, res) => {
-  //query params are ALWAYS strings
-  const id = parseInt(req.params.id);
-  const psyFound = psyList.find((psy) => psy.id === id);
-  if (!psyFound) {
-    return res
-      .status(404)
-      .json({ message: `Psychologist not found with id: ${id}` });
-  }
-  res.json(psyFound);
+	const id = parseInt(req.params.id);
+	const psyFound = psyList.find((psy) => psy.id === id);
+	if (!psyFound) {
+		return res.status(404).json({ message: `Psychologist not found with id: ${id}` });
+	}
+	res.json(psyFound);
 };
 
 const getByName = (req, res) => {
-  const psyFound = psyList.filter(
-    (psy) =>
-      psy.firstName === req.query.firstName &&
-      psy.lastName === req.query.lastName
-  );
-  if (psyFound.length <= 0) {
-    return res.status(404).json({
-      message: `Psychologist not found with name: ${psyFound.firstName} ${psyFound.lastName}`,
-    });
-  }
-  res.json(psyFound);
+	const psyFound = psyList.filter(
+		(psy) => psy.firstName === req.query.firstName && psy.lastName === req.query.lastName
+	);
+	if (psyFound.length <= 0) {
+		return res.status(404).json({
+			message: `Psychologist not found with name: ${psyFound.firstName} ${psyFound.lastName}`,
+		});
+	}
+	res.json(psyFound);
 };
 
 const add = (req, res) => {
-  const newPsy = {
-    id: getLastId(psyList) + 1,
-    firstName: req.query.firstName,
-    lastName: req.query.lastName,
-    /*     email: req.query.email,
-    pictureUrl: req.query.pictureUrl,
-    isActive: req.query.isActive,
-    password: req.query.password, */
-    turns: [],
-  };
-  if (!validate(newPsy)) {
-    return res.status(400).json({ message: "Missing parameters" });
-  }
-  psyList.push(newPsy);
-  fs.writeFile(
-    path.join(__dirname, "../data/psychologists.json"),
-    JSON.stringify(psyList),
-    (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({ message: "Error adding Psy" });
-        return;
-      }
-      res.json({ message: "Psychologist added successfully", psy: newPsy });
-    }
-  );
+	const newPsy = {
+		id: getLastId(psyList) + 1,
+		firstName: req.query.firstName,
+		lastName: req.query.lastName,
+		email: req.query.email,
+		pictureUrl: req.query.pictureUrl,
+		isActive: req.query.isActive,
+		password: req.query.password,
+		turns: [],
+	};
+	if (!validate(newPsy)) {
+		return res.status(400).json({ message: "Missing parameters" });
+	}
+	psyList.push(newPsy);
+	fs.writeFile(path.join(__dirname, "../data/psychologists.json"), JSON.stringify(psyList), (err) => {
+		if (err) {
+			console.log(err);
+			res.status(500).json({ message: "Error adding Psy" });
+			return;
+		}
+		res.json({ message: "Psychologist added successfully", psy: newPsy });
+	});
 };
 
 const edit = (req, res) => {
-  const id = parseInt(req.params.id);
-  const psyFound = psyList.find((psy) => psy.id === id);
-  if (!psyFound) {
-    return res.status(404).json({ message: `psy not found with id ${id}` });
-  }
-  psyList = psyList.map((psy) => {
-    if (psy.id === id) {
-      psy.firstName = req.query.firstName || psy.firstName;
-      psy.lastName = req.query.lastName || psy.lastName;
-      psy.email = req.query.email || psy.email;
-      psy.pictureUrl = req.query.pictureUrl || psy.pictureUrl;
-      psy.isActive = req.query.isActive || psy.isActive;
-      psy.password = req.query.password || psy.password;
-      psy.turns = req.query.turns || [];
-    }
-    return psy;
-  });
-  fs.writeFile(
-    path.join(__dirname, "../data/psychologists.json"),
-    JSON.stringify(psyList),
-    (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ message: "Error edting psychologist" });
-      }
-      res.json({ message: "Psychologist edited successfully", psyFound });
-    }
-  );
+	const id = parseInt(req.params.id);
+	const psyFound = psyList.find((psy) => psy.id === id);
+	if (!psyFound) {
+		return res.status(404).json({ message: `Psychologist not found with id ${id}` });
+	}
+	psyList = psyList.map((psy) => {
+		if (psy.id === id) {
+			psy.firstName = req.query.firstName || psy.firstName;
+			psy.lastName = req.query.lastName || psy.lastName;
+			psy.email = req.query.email || psy.email;
+			psy.pictureUrl = req.query.pictureUrl || psy.pictureUrl;
+			psy.isActive = req.query.isActive || psy.isActive;
+			psy.password = req.query.password || psy.password;
+			psy.turns = [];
+		}
+		return psy;
+	});
+	fs.writeFile(path.join(__dirname, "../data/psychologists.json"), JSON.stringify(psyList), (err) => {
+		if (err) {
+			console.log(err);
+			return res.status(500).json({ message: "Error editing psychologist" });
+		}
+		res.json({ message: "Psychologist edited successfully", psyFound });
+	});
 };
 
 const remove = (req, res) => {
-  const id = parseInt(req.params.id);
-  const psyFound = psyList.find((psy) => psy.id === id);
-  if (!psyFound) {
-    return res
-      .status(404)
-      .json({ message: `Psychologist not found with id ${id}` });
-  }
-  psyList = psyList.filter((psy) => psy.id !== id);
-  fs.writeFile(
-    path.join(__dirname, "../data/psychologists.json"),
-    JSON.stringify(psyList),
-    (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ message: "Error deleting psychologist" });
-      }
-      res.json({ message: "Psychologist deleted", psy: psyFound });
-    }
-  );
+	const id = parseInt(req.params.id);
+	const psyFound = psyList.find((psy) => psy.id === id);
+	if (!psyFound) {
+		return res.status(404).json({ message: `Psychologist not found with id ${id}` });
+	}
+	psyList = psyList.filter((psy) => psy.id !== id);
+	fs.writeFile(path.join(__dirname, "../data/psychologists.json"), JSON.stringify(psyList), (err) => {
+		if (err) {
+			console.log(err);
+			return res.status(500).json({ message: "Error deleting psychologist" });
+		}
+		res.json({ message: "Psychologist deleted", psy: psyFound });
+	});
 };
 
 module.exports = {
-  getAll: getAll,
-  getById: getById,
-  getByName: getByName,
-  add: add,
-  edit: edit,
-  remove: remove,
+	getAll: getAll,
+	getById: getById,
+	getByName: getByName,
+	add: add,
+	edit: edit,
+	remove: remove,
 };
--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  let html = "<h1>MindeSet</h1>";
-  html += "<ul>";
-  html += "  <li>psychologists:";
-  html += "    <ul>";
-  html += '      <li><a href="/psychologists">getAll</a></li>';
-  html += '      <li><a href="/psychologists/1">getById</a></li>';
-  html +=
-    '      <li><a href="/psychologists/byName?first_name=Mirvin&last_name=Dowdeswell">getByName</a></li>';
-  html += '      <li><a href="/psychologists/add">add</a></li>';
-  html += '      <li><a href="/psychologists/edit">edit</a></li>';
-  html += '      <li><a href="/psychologists/remove">remove</a></li>';
-  html += "    </ul>";
-  html += "  </li>";
-  html += "</ul>";
-  res.send(html);
+	let html = "<h1>MindSet</h1>";
+	html += "<ul>";
+	html += "  <li>psychologists:";
+	html += "    <ul>";
+	html += '      <li><a href="/psychologists">getAll</a></li>';
+	html += '      <li><a href="/psychologists/1">getById</a></li>';
+	html += '      <li><a href="/psychologists/byName?firstName=Mirvin&lastName=Dowdeswell">getByName</a></li>';
+	html += '      <li><a href="/psychologists/add">add</a></li>';
+	html += '      <li><a href="/psychologists/edit/1">edit</a></li>';
+	html += '      <li><a href="/psychologists/remove/2">remove</a></li>';
+	html += "    </ul>";
+	html += "  </li>";
+	html += "</ul>";
+	res.send(html);
 });
 
 app.get("/psychologists", psychologists.getAll);
@@ -31,5 +30,5 @@ app.get("/psychologists/byName", psychologists.getByName);
 app.get("/psychologists/:id", psychologists.getById);
 
 app.listen(port, () => {
-  console.log(`Open your browser in http://localhost:${port}`);
+	console.log(`Open your browser in http://localhost:${port}`);
 });
