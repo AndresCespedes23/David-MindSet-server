1f58e6fed68a3acf0601eb7dbbaad415839093be
Valetín Feraudo <valentinferaudo@gmail.com>
2021-11-08 23:18:51 -0300

    Fix Agustin review 2

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -27,9 +27,7 @@ const getById = (req, res) => {
     const candidateID = parseInt(req.params.id);
     const foundCandidate = candidates.list.find(candidate => candidate.id === candidateID);
     if(!foundCandidate){
-        return res.status(404).json({
-            message:`Candidate not found with id: ${candidateID}`
-        });
+        return res.status(404).json({message:`Candidate not found with id: ${candidateID}`});
     }
     res.json(foundCandidate);
 };
@@ -69,24 +67,20 @@ const add = (req, res) => {
         profileTypes: req.query.profileTypes === undefined ? [] : [req.query.profileTypes] ,
         isOpenToWork: true,
         isActive: true
-    }
+    };
     if (!validate(newCandidate)) {
-        return res.status(400).json({ message: 'Missing parameters' });
+        return res.status(400).json({message: 'Missing parameters' });
     }
     candidates.list.push(newCandidate);
     fs.writeFile(path.join(__dirname, '../data/candidates.json'), JSON.stringify(candidates),err =>{
         if(err){
-            res.status(500).json({
-                message:'Error while saving data'
-            })
+            return res.status(500).json({message:'Error while saving data'});
         }
-        else{
-            res.json({
-                message:'Candidate created',
-                candidate: newCandidate
-            })
-        }
-    })
+        res.json({
+            message:'Candidate created',
+            candidate: newCandidate
+        });
+    });
 };
 
 const edit = (req, res) => {
@@ -94,73 +88,57 @@ const edit = (req, res) => {
     const foundCandidate = candidates.list.find(candidate => candidate.id === candidateID);
     let updatedCandidate;
     if(!foundCandidate){
-        res.status(404).json({
-            message:`Candidate not found with id: ${candidateID}`
-        });
+        return res.status(404).json({message:`Candidate not found with id: ${candidateID}`});
     }
-    else{
-        candidates = candidates.list.map((candidate) => {
-            if(candidate.id === candidateID){
-                candidate.firstName =  req.query.firstName || candidate.firstName;
-                candidate.lastName =  req.query.lastName || candidate.lastName;
-                candidate.email =  req.query.email || candidate.email;
-                candidate.password =  req.query.password || candidate.password;
-                candidate.pictureUrl =  req.query.pictureUrl || candidate.pictureUrl;
-                candidate.phone =  req.query.phone || candidate.phone;
-                candidate.address =  req.query.address || candidate.address;
-                candidate.city =  req.query.city || candidate.city;
-                candidate.province =  req.query.province || candidate.province;
-                candidate.country =  req.query.country || candidate.country;
-                candidate.postalCode =  req.query.postalCode || candidate.postalCode;
-                candidate.birthday =  req.query.birthday || candidate.birthday;
-                candidate.education = req.query.education !== undefined ? [req.query.education] : candidate.education;
-                candidate.experiences = req.query.experiences !== undefined ? [req.query.experiences] : candidate.experiences;
-                candidate.courses = req.query.courses !== undefined ? [req.query.courses] : candidate.courses;
-                candidate.hobbies = req.query.hobbies !== undefined ? [req.query.hobbies] : candidate.hobbies;
-                candidate.mainSkills = req.query.mainSkills !== undefined ? [req.query.mainSkills] : candidate.mainSkills;
-                candidate.profileTypes = req.query.profileTypes !== undefined ? [req.query.profileTypes] : candidate.profileTypes;
-                candidate.isOpenToWork = req.query.isOpenToWork || candidate.isOpenToWork;
-                updatedCandidate = candidate;
-            }
-            return candidate;
+    candidates = candidates.list.map((candidate) => {
+        if(candidate.id === candidateID){
+            candidate.firstName =  req.query.firstName || candidate.firstName;
+            candidate.lastName =  req.query.lastName || candidate.lastName;
+            candidate.email =  req.query.email || candidate.email;
+            candidate.password =  req.query.password || candidate.password;
+            candidate.pictureUrl =  req.query.pictureUrl || candidate.pictureUrl;
+            candidate.phone =  req.query.phone || candidate.phone;
+            candidate.address =  req.query.address || candidate.address;
+            candidate.city =  req.query.city || candidate.city;
+            candidate.province =  req.query.province || candidate.province;
+            candidate.country =  req.query.country || candidate.country;
+            candidate.postalCode =  req.query.postalCode || candidate.postalCode;
+            candidate.birthday =  req.query.birthday || candidate.birthday;
+            candidate.education = req.query.education !== undefined ? [req.query.education] : candidate.education;
+            candidate.experiences = req.query.experiences !== undefined ? [req.query.experiences] : candidate.experiences;
+            candidate.courses = req.query.courses !== undefined ? [req.query.courses] : candidate.courses;
+            candidate.hobbies = req.query.hobbies !== undefined ? [req.query.hobbies] : candidate.hobbies;
+            candidate.mainSkills = req.query.mainSkills !== undefined ? [req.query.mainSkills] : candidate.mainSkills;
+            candidate.profileTypes = req.query.profileTypes !== undefined ? [req.query.profileTypes] : candidate.profileTypes;
+            candidate.isOpenToWork = req.query.isOpenToWork || candidate.isOpenToWork;
+            updatedCandidate = candidate;
+        }
+        return candidate;
+    });
+    fs.writeFile(path.join(__dirname, '../data/candidates.json'), JSON.stringify({list:candidates}),err =>{
+        if(err){
+            return res.status(500).json({message:'Error while saving data'});
+        }
+        res.json({
+            message: 'Updated Candidate',
+            candidate: updatedCandidate
         });
-        fs.writeFile(path.join(__dirname, '../data/candidates.json'), JSON.stringify({list:candidates}),err =>{
-            if(err){
-                res.status(500).json({
-                    message:'Error while saving data'
-                });
-            }
-            else{
-                res.json({
-                    message: 'Updated Candidate',
-                    candidate: updatedCandidate
-                });
-            }
-        })
-    }
+    });
 };
 
 const remove = (req, res) => {
     const candidateID = parseInt(req.params.id);
     const foundCandidate = candidates.list.filter(candidate => candidate.id === candidateID);
     if(!foundCandidate){
-        return res.status(404).json({
-            message:`Candidate not found with id: ${candidateID}`
-        });
+        return res.status(404).json({message:`Candidate not found with id: ${candidateID}`});
     }
     candidates = candidates.list.filter((candidate) => candidate.id !== candidateID);
     fs.writeFile(path.join(__dirname, '../data/candidates.json'), JSON.stringify({list:candidates}),err =>{
         if(err){
-            res.status(500).json({
-                message:'Error while saving data'
-            })
+            return res.status(500).json({message:'Error while saving data'});
         }
-        else{
-            res.json({
-                message: 'Deleted Candidate'
-            })
-        }
-    })
+        res.json({message: 'Deleted Candidate'});
+    });
 };
 
 module.exports = {
