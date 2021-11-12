2067e16df6e634ab827ea854eac87c490c4d0a9d
AlfonsoScornavacca <alfondrums@gmail.com>
2021-11-12 17:52:51 -0300

    Changes on applications routes, models and index

---
+++ models/Applications.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+
+const ApplicationsSchema = new Schema({
+    id: Schema.Types.ObjectId,
+    idCandidate: { type: Schema.Types.ObjectId, ref: 'Candidate' },
+    idOpenPosition: { type: Schema.Types.ObjectId, ref: 'Open Position' },
+    isActive: { type: Boolean, default: true },
+})
+
+module.exports = model('Applications', ApplicationsSchema);
