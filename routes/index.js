48f0f8a675941279de4b0760515981723a4002cc
Valetín Feraudo <valentinferaudo@gmail.com>
2021-11-12 16:23:48 -0300

    First commit. GetAll and getByID

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const psychologists = require('./psychologists');
 
 const router = express.Router();
@@ -8,13 +8,6 @@ router.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../public') });
 });
 
-router.get('/server-status', (req, res) =>
-  res.send({
-    status: 'Server OK',
-  })
-);
-
 router.use('/psychologists', psychologists);
 
 module.exports = router;
-
