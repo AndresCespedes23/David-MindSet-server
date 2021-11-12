2b8f5419f2fd0c016bd009e343c334d6b7d5f675
Valetín Feraudo <valentinferaudo@gmail.com>
2021-11-12 17:05:43 -0300

    GetByName and Add

--- controllers/psychologists.js
+++ controllers/psychologists.js
@@ -9,18 +9,39 @@ const getAll = (req, res) => {
 
 const getById = (req, res) => {
   const { id } = req.param;
-  Psychologists.find()
+  Psychologists.findById()
     .then((pyschologist) => {
-      res.json({ pyschologist });
+      if (!pyschologist) {
+        return res.status(404).json({ msg: `Psychologists not found by ID: ${id}` });
+      }
+      return res.json({ pyschologist });
     })
-    .catch(() => res.status(400).json({ msg: 'Psychologists not found' }));
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
 const getByName = (req, res) => {
+  const { name } = req.param;
+  Psychologist.find({ name: name.toLowerCase() })
+    .then((psychologists) => res.json({ psychologists }))
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
 const add = (req, res) => {
-
+  const newPsychologist = new Psychologist({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    pictureUrl: req.body.pictureUrl,
+    turns: [],
+    isActive: true,
+  });
+  newPsychologist.save((err, psychologist) => {
+    if (err) {
+      return res.status(400).json({ msg: `Error: ${err}` });
+    }
+    return res.json({ msg: 'Psychologist created', psychologist });
+  });
 };
 
 const edit = (req, res) => {
