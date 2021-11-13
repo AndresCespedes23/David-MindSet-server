9536a8cc00c42f87a78fafddaec06eb0dd9d168d
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-13 14:05:36 -0300

    set requierd to properties of model

--- models/Open-position.js
+++ models/Open-position.js
@@ -4,10 +4,14 @@ const { Schema, model } = mongoose;
 
 const OpenPositionstSchema = new Schema({
   id: Schema.Types.ObjectId,
-  idCompany: { type: Schema.Types.ObjectId, ref: 'Company' },
-  startDate: { type: Date, minlength: 8, maxlength: 8 },
+  idCompany: { type: Schema.Types.ObjectId, ref: 'Company', required: true },
+  startDate: {
+    type: Date, minlength: 8, maxlength: 8, required: true,
+  },
   endDate: { type: Date, minlength: 8, maxlength: 8 },
-  jobDescription: { type: String, minlength: 10, maxlength: 1000 },
+  jobDescription: {
+    type: String, minlength: 10, maxlength: 1000, required: true,
+  },
   isActive: { type: Boolean, default: true },
 });
 
