02ac0e10f92ad6b94d23c0194b6e3899e482d86c
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-13 17:37:21 -0300

    add controllers add function

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -7,12 +7,24 @@ const getAll = (req, res) => {
 };
 
 const getById = (req, res) => {
+
 };
 
 const getByCompany = (req, res) => {
 };
 
 const add = (req, res) => {
+  const newInterview = new Interview({
+    idCompany: req.body.idCompany,
+    idCandidate: req.body.idCandidate,
+    date: req.body.date,
+    status: req.body.status,
+    isActive: true,
+  });
+  newInterview
+    .save()
+    .then((data) => res.json({ msg: 'New interview added', data }))
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
 const edit = (req, res) => {
