bb2055754ed1e1b951bab1dd5ec30e4d89bce84e
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-13 02:07:44 -0300

    getById and getByName functions completed

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -1,17 +1,33 @@
-const candidatesModel = require('../models/Candidates');
+const Candidates = require('../models/Candidates');
 
 const getAll = (req, res) => {
-  candidatesModel.find()
+  Candidates.find()
     .then((candidates) => res.status(200).json({ candidates }))
     .catch((error) => res.status(400).json({ msg: `Error: ${error}` }));
 };
 
-const getById = () => {
-  console.log('getById');
+const getById = (req, res) => {
+  const { id } = req.params;
+  Candidates.findById(id)
+    .then((data) => {
+      if (!data) {
+        return res.status(404).json({ msg: `No candidate with the id of ${id} founded` });
+      }
+      return res.status(200).json({ data });
+    })
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
-const getByName = () => {
-  console.log('getByName');
+const getByName = (req, res) => {
+  const { name } = req.params;
+  Candidates.find({ firstName: name })
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ msg: `No candidate with the name of ${name} founded` });
+      }
+      return res.status(200).json({ data });
+    })
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
 const add = () => {
