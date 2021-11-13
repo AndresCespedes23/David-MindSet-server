184ea29539b3e9f6ba916503ad5976e338712e24
Sofía Pagura <sofiapagura@gmail.com>
2021-11-12 22:48:27 -0300

    Create schema

---
+++ models/Administrators.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const AdministratorsSchema = new Schema({
+
+  id: Schema.Types.ObjectId,
+  firstName: {
+    type: String, required: true, minLenght: 2, maxLength: 40,
+  },
+  lastName: {
+    type: String, required: true, minLenght: 2, maxLength: 40,
+  },
+  email: {
+    type: String, required: true, minLenght: 5, maxLength: 40,
+  },
+  password: {
+    type: String, required: true, minLenght: 8, maxLength: 16,
+  },
+  isActive: { type: Boolean, default: true },
+});
+
+module.exports = model('Administrators', AdministratorsSchema);
--- routes/administrators.js
+++ routes/administrators.js
@@ -5,9 +5,9 @@ const administrators = require('../controllers/administrators');
 // tmb carpeta de funciones
 
 router.get('/', administrators.getAll);
-router.post('/', administrators.add);
-router.put('/:id', administrators.edit);
-router.delete('/:id', administrators.remove);
+router.post('/add', administrators.add);
+router.put('/edit/:id', administrators.edit);
+router.delete('/remove/:id', administrators.remove);
 router.get('/byName', administrators.getByName);
 router.get('/:id', administrators.getById);
 
