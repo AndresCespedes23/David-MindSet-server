d7e41b00af5bd17fa147794dbda70ee3246ce4ad
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-12 22:59:15 -0300

    path modified into the getByName router

--- routes/candidates.js
+++ routes/candidates.js
@@ -7,7 +7,7 @@ router.get('/', candidates.getAll);
 router.post('/add', candidates.add);
 router.put('/edit/:id', candidates.edit);
 router.delete('/remove/:id', candidates.remove);
-router.get('/byName', candidates.getByName);
+router.get('/byName/:name', candidates.getByName);
 router.get('/:id', candidates.getById);
 
 module.exports = router;
