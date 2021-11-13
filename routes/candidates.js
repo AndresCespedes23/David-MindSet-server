8d53b6906b8200ba0977044d5eee4049703987c2
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-13 16:44:41 -0300

    action of the requests removed at candidates routes file

--- routes/candidates.js
+++ routes/candidates.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const candidates = require('../controllers/candidates');
 
 router.get('/', candidates.getAll);
-router.post('/add', candidates.add);
-router.put('/edit/:id', candidates.edit);
-router.delete('/remove/:id', candidates.remove);
+router.post('/', candidates.add);
+router.put('/:id', candidates.edit);
+router.delete('/:id', candidates.remove);
 router.get('/byName/:name', candidates.getByName);
 router.get('/:id', candidates.getById);
 
