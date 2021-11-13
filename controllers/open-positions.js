a727c3cada084efbd0c6e958b2d39b37d297c12b
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-12 23:25:42 -0300

    getById and getByCompanyId donde

--- controllers/open-positions.js
+++ controllers/open-positions.js
@@ -1,18 +1,33 @@
-const openPositions = require('../models/Open-position');
+const OpenPositions = require('../models/Open-position');
 
 const getAll = (req, res) => {
-  openPositions
-    .find()
+  OpenPositions.find()
     .then((data) => res.status(200).json({ data }))
     .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
-const getById = () => {
-  console.log('byid');
+const getById = (req, res) => {
+  const { id } = req.params;
+  OpenPositions.findById(id)
+    .then((data) => {
+      if (!data) {
+        return res.status(404).json({ msg: `Open Position not found by ID: ${id}` });
+      }
+      return res.status(200).json({ data });
+    })
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
-const getByIdCompany = () => {
-  console.log('byCompanyId');
+const getByIdCompany = (req, res) => {
+  const { id } = req.params;
+  OpenPositions.find({ idCompany: id })
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ msg: `Open Position not found by Company ID: ${id}` });
+      }
+      return res.status(200).json({ data });
+    })
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
 const add = () => {
