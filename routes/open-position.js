86b6c1d41b93a58f1252b95241acff686f60c5d5
Julian Vicente <julian.vicente97s@gmail.com>
2021-11-14 01:52:21 -0300

    check length validator

--- routes/open-position.js
+++ routes/open-position.js
@@ -6,7 +6,7 @@ const openPositionsValidator = require('../validators/open-positions');
 
 router.get('/', openPositions.getAll);
 router.get('/byIdCompany/:id', openPositionsValidator.isObjectID, openPositions.getByIdCompany);
-router.post('/', openPositionsValidator.isNotEmpty, openPositions.add);
+router.post('/', openPositionsValidator.isNotEmpty, openPositionsValidator.validateLength, openPositions.add);
 router.put('/:id', openPositionsValidator.isObjectID, openPositions.edit);
 router.delete('/:id', openPositionsValidator.isObjectID, openPositions.remove);
 router.get('/:id', openPositionsValidator.isObjectID, openPositions.getById);
--- validators/open-positions.js
+++ validators/open-positions.js
@@ -21,7 +21,25 @@ const isObjectID = (req, res, next) => {
   return next();
 };
 
+const checkLength = (word, minLength, maxLength) => {
+  if (minLength <= word.length && word.length <= maxLength) {
+    return true;
+  }
+  return false;
+};
+
+const validateLength = (req, res, next) => {
+  if (!checkLength(req.body.startDate, 10, 10)) {
+    return res.status(400).json({ msg: 'Start date should have 10 characters' });
+  }
+  if (!checkLength(req.body.jobDescription, 10, 500)) {
+    return res.status(400).json({ msg: 'Job description must be between 10 and 500 characters' });
+  }
+  return next();
+};
+
 module.exports = {
   isNotEmpty,
   isObjectID,
+  validateLength,
 };
