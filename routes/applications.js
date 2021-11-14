cd5c8a03eaef16020efdbbdb7e65e197c513a853
AlfonsoScornavacca <alfondrums@gmail.com>
2021-11-14 20:33:17 -0300

    postman collection created

--- routes/applications.js
+++ routes/applications.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const applications = require('../controllers/applications');
-const { validateFormat, isNotEmpty, validateLength } = require('../validators/applications');
+const { validateFormat, isNotEmpty } = require('../validators/applications');
 
 router.get('/', applications.getAll);
 router.get('/search', validateFormat, applications.search);
 router.get('/byCan/:id', validateFormat, applications.getByCandidate);
 router.post('/', isNotEmpty, validateFormat, applications.add);
-router.put('/:id', validateFormat, validateLength, applications.edit);
+router.put('/:id', validateFormat, applications.edit);
 router.delete('/:id', applications.remove);
 router.get('/:id', validateFormat, applications.getById);
 module.exports = router;
