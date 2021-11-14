e4a158177ecf5ddee117a5bb496467911a4c99af
AlfonsoScornavacca <alfondrums@gmail.com>
2021-11-13 21:00:20 -0300

    getByPosition function created

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,26 +1,7 @@
 let fs = require('fs');
 const path = require('path');
 const Applications = require('../models/Applications');
-/*
-const getLastId = (collection) => {
-    let larger = 0;
-    collection.forEach((element) => {
-        if (element.id > larger) {
-            larger = element.id;
-        }
-    });
-    return larger;
-};
 
-const validate = (object) => {
-    for (let key in object) {
-        if (object[key] === undefined) {
-            return false;
-        }
-    }
-    return true;
-};
-*/
 const getAll = (req, res) => {
     Applications.find()
         .then((applications) => {
@@ -42,45 +23,22 @@ const getByCandidate = (req, res) => {
     })
     .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
-/*
+
 const getByPosition = (req, res) => {
-    const id = parseInt(req.params.id);
-    const application = applications.filter((applications) => applications.idOpenPosition === id);
-    if (application === undefined) {
-        return res.status(404).json({ message: `no applications with open position id: ${id}` });
-    }
-    res.json(application);
-};
-
-const getByCandidate = (req, res) => {
-    const id = parseInt(req.params.id);
-    const application = applications.filter((applications) => applications.idCandidate === id);
-    if (application === undefined) {
-        return res.status(404).json({ message: `no applications with candidate id: ${id}` });
-    }
-    res.json(application);
-};
-
-const add = (req, res) => {
-    let newId = getLastId(applications) + 1;
-    let newItem = {
-        id: newId,
-        idCandidate: req.query.idCandidate,
-        idOpenPosition: req.query.idOpenPosition,
-        isActive: true
-    }
-    if (!validate(newItem)) {
-        return res.status(400).json({ message: 'no data in query' });
-    }
-    applications.push(newItem);
-    fs.writeFile(path.join(__dirname, '../data/applications.json'), JSON.stringify(applications), (err) => {
-        if (err) {
-            return res.status(500).json({ message: 'error adding application' })
-        };
-        res.json({ message: 'Application added' });
+    const { id } = req.params;
+    Applications.find({ idPosition: id })
+    .then((data) => {
+        if (data.length === 0) {
+            return res.status(404).json({ msg: `Position not found by Position ID: ${id}` });
+        }
+        return res.json ({ data });
     })
+    .catch((err) => res.status(400).json({ msg: `Error: ${err}` }));
 };
 
+
+
+/*
 const edit = (req, res) => {
     const id = parseInt(req.params.id);
     if (!id) {
@@ -122,11 +80,11 @@ const remove = (req, res) => {
 };*/
 
 module.exports = {
-    getAll: getAll
-   /* getById: getById,
+    getAll: getAll,
+    //getById: getById,
     getByPosition: getByPosition,
     getByCandidate: getByCandidate,
-    add: add,
-    edit: edit,
-    remove: remove*/
+    //add: add,
+    //edit: edit,
+   // remove: remove
 };
