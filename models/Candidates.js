3f987b49a17ff3737e7a1bfaa02063eb040af80f
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-14 17:23:47 -0300

    consistency fixed in candidates model

--- models/Candidates.js
+++ models/Candidates.js
@@ -5,75 +5,183 @@ const { Schema, model } = mongoose;
 const CandidatesSchema = new Schema({
   id: Schema.Types.ObjectId,
   firstName: {
-    type: String, required: true, minLenght: 2, maxLength: 40,
+    type: String,
+    required: true,
+    minLenght: 2,
+    maxLength: 40,
   },
   lastName: {
-    type: String, required: true, minLenght: 2, maxLength: 40,
+    type: String,
+    required: true,
+    minLenght: 2,
+    maxLength: 40,
   },
   email: {
-    type: String, required: true, minLenght: 5, maxLength: 40,
+    type: String,
+    required: true,
+    minLenght: 5,
+    maxLength: 40,
   },
   password: {
-    type: String, required: true, minLenght: 8, maxLength: 16,
+    type: String,
+    required: true,
+    minLenght: 8,
+    maxLength: 16,
   },
-  pictureUrl: { type: String, default: null },
+  pictureUrl: {
+    type: String,
+    default: null,
+  },
   phone: {
-    type: Number, required: true, min: 1000000, max: 10000000,
+    type: Number,
+    required: true,
+    min: 1000000,
+    max: 10000000,
   },
   address: {
     street: {
-      type: String, required: true, minlength: 3, maxlength: 40,
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 40,
     },
     number: {
-      type: Number, required: true, min: 10, max: 10000,
+      type: Number,
+      required: true,
+      min: 10,
+      max: 10000,
     },
   },
   city: {
-    type: String, required: true, minlength: 3, maxlength: 40,
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 40,
   },
   province: {
-    type: String, required: true, minlength: 3, maxlength: 40,
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 40,
   },
   country: {
-    type: String, required: true, minlength: 3, maxlength: 40,
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 40,
   },
   postalCode: {
-    type: Number, required: true, min: 1, max: 10000,
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10000,
   },
   birthday: {
-    type: Date, required: true, minlength: 10, maxlength: 10,
+    type: Date,
+    required: true,
+    minlength: 10,
+    maxlength: 10,
   },
   education: [{
-    institution: { type: String, minlength: 3, maxlength: 40 },
-    city: { type: String, minlength: 3, maxlength: 40 },
-    state: { type: String, minlength: 3, maxlength: 40 },
-    graduationYear: { type: Number, min: 1, max: 10000 },
-    description: { type: String, minlength: 3, maxlength: 40 },
+    institution: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    city: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    state: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    graduationYear: {
+      type: Number,
+      min: 1,
+      max: 10000,
+    },
+    description: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
   }],
   experiences: [{
-    position: { type: String, minlength: 3, maxlength: 40 },
-    company: { type: String, minlength: 3, maxlength: 40 },
-    since: { type: Date, minlength: 8, maxlength: 8 },
-    until: { type: Date, minlength: 8, maxlength: 8 },
-    jobDescription: { type: String, minlength: 3, maxlength: 40 },
+    position: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    company: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    since: {
+      type: Date,
+      minlength: 8,
+      maxlength: 8,
+    },
+    until: {
+      type: Date,
+      minlength: 8,
+      maxlength: 8,
+    },
+    jobDescription: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
   }],
   courses: [{
-    tittle: { type: String, minlength: 3, maxlength: 40 },
-    institution: { type: String, minlength: 3, maxlength: 40 },
-    year: { type: Number, min: 1, max: 10000 },
+    tittle: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    institution: {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
+    year: {
+      type: Number,
+      min: 1,
+      max: 10000,
+    },
   }],
   hobbies: [
-    { type: String, minlength: 3, maxlength: 40 },
+    {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
   ],
   mainSkills: [
-    { type: String, minlength: 3, maxlength: 40 },
+    {
+      type: String,
+      minlength: 3,
+      maxlength: 40,
+    },
   ],
   profileTypes: [
-    { type: Schema.Types.ObjectId, ref: 'profileTypesSchema' },
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'profileTypesSchema',
+    },
   ],
-  isOpenToWork: { type: Boolean, default: false },
+  isOpenToWork: {
+    type: Boolean,
+    default: false,
+  },
 
-  isActive: { type: Boolean, default: true },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 module.exports = model('Candidates', CandidatesSchema);
