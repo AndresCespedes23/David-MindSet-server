c2c661ba15ce3c26280f4cd6435a1a87ad201848
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-14 13:05:29 -0300

    minor changes to the candidates rountes file

--- routes/candidates.js
+++ routes/candidates.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const candidates = require('../controllers/candidates');
-const candidatesValidator = require('../validators/candidates');
+const { isNotEmpty, isObjectID, validateLength } = require('../validators/candidates');
 
 router.get('/', candidates.getAll);
-router.post('/', candidatesValidator.isNotEmpty, candidatesValidator.validateLength, candidates.add);
-router.put('/:id', candidatesValidator.isObjectID, candidates.edit);
-router.delete('/:id', candidatesValidator.isObjectID, candidates.remove);
 router.get('/byName/:name', candidates.getByName);
-router.get('/:id', candidatesValidator.isObjectID, candidates.getById);
+router.get('/:id', isObjectID, candidates.getById);
+router.post('/', isNotEmpty, validateLength, candidates.add);
+router.put('/:id', isObjectID, candidates.edit);
+router.delete('/:id', isObjectID, candidates.remove);
 
 module.exports = router;
