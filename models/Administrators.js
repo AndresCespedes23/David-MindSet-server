f7520f8d9fdf567af90d249ccb55eb71c08680b0
Sofía Pagura <sofiapagura@gmail.com>
2021-11-14 19:48:13 -0300

    Fix sintax in /models

--- models/Administrators.js
+++ models/Administrators.js
@@ -3,21 +3,36 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const AdministratorsSchema = new Schema({
-
   id: Schema.Types.ObjectId,
   firstName: {
-    type: String, required: true, minLenght: 2, maxLength: 40, // here u cant put more validations
+    type: String,
+    required: true,
+    minLenght: 2,
+    maxLength: 40,
+    // here u cant put more validations
   },
   lastName: {
-    type: String, required: true, minLenght: 2, maxLength: 40,
+    type: String,
+    required: true,
+    minLenght: 2,
+    maxLength: 40,
   },
   email: {
-    type: String, required: true, minLenght: 5, maxLength: 40,
+    type: String,
+    required: true,
+    minLenght: 5,
+    maxLength: 40,
   },
   password: {
-    type: String, required: true, minLenght: 8, maxLength: 16,
+    type: String,
+    required: true,
+    minLenght: 8,
+    maxLength: 16,
   },
-  isActive: { type: Boolean, default: true },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 module.exports = model('Administrators', AdministratorsSchema);
