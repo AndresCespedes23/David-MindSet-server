f098670313f9d90663060cf0ab587f247d271a7e
Valetín Feraudo <valentinferaudo@gmail.com>
2021-11-15 10:52:06 -0300

    Fix validators empty bug

--- validators/psychologists.js
+++ validators/psychologists.js
@@ -26,33 +26,49 @@ const isNotEmpty = (req, res, next) => {
 };
 
 const validateFormat = (req, res, next) => {
-  if (!typeof (req.body.firstName) === 'string') {
-    return res.status(400).json({ msg: 'First Name must be string' });
+  if (req.body.firstName !== undefined) {
+    if (!typeof (req.body.firstName) === 'string') {
+      return res.status(400).json({ msg: 'First Name must be string' });
+    }
   }
-  if (!typeof (req.body.lastName) === 'string') {
-    return res.status(400).json({ msg: 'Last Name must be string' });
+  if (req.body.lastName !== undefined) {
+    if (!typeof (req.body.lastName) === 'string') {
+      return res.status(400).json({ msg: 'Last Name must be string' });
+    }
   }
-  if (!(req.body.email.split('').indexOf('@') !== -1 && req.body.email.split('').indexOf('.') !== -1)) {
-    return res.status(400).json({ msg: 'Email must be an email format' });
+  if (req.body.email !== undefined) {
+    if (!(req.body.email.split('').indexOf('@') !== -1 && req.body.email.split('').indexOf('.') !== -1)) {
+      return res.status(400).json({ msg: 'Email must be an email format' });
+    }
   }
-  if (!typeof (req.body.password) === 'string') {
-    return res.status(400).json({ msg: 'Password must be string' });
+  if (req.body.password !== undefined) {
+    if (!typeof (req.body.password) === 'string') {
+      return res.status(400).json({ msg: 'Password must be string' });
+    }
   }
   return next();
 };
 
 const validateLength = (req, res, next) => {
-  if (!(req.body.firstName.length >= 2 && req.body.firstName.length <= 40)) {
-    return res.status(400).json({ msg: 'First Name must be between 2 and 40 characters' });
+  if (req.body.firstName !== undefined) {
+    if (!(req.body.firstName.length >= 2 && req.body.firstName.length <= 40)) {
+      return res.status(400).json({ msg: 'First Name must be between 2 and 40 characters' });
+    }
   }
-  if (!(req.body.lastName.length >= 2 && req.body.lastName.length <= 40)) {
-    return res.status(400).json({ msg: 'Last Name must be between 2 and 40 characters' });
+  if (req.body.lastName !== undefined) {
+    if (!(req.body.lastName.length >= 2 && req.body.lastName.length <= 40)) {
+      return res.status(400).json({ msg: 'Last Name must be between 2 and 40 characters' });
+    }
   }
-  if (!(req.body.email.length >= 5 && req.body.email.length <= 50)) {
-    return res.status(400).json({ msg: 'Email must be between 2 and 50 characters' });
+  if (req.body.email !== undefined) {
+    if (!(req.body.email.length >= 5 && req.body.email.length <= 50)) {
+      return res.status(400).json({ msg: 'Email must be between 2 and 50 characters' });
+    }
   }
-  if (!(req.body.password.length >= 8 && req.body.password.length <= 16)) {
-    return res.status(400).json({ msg: 'Password must be between 8 and 16 characters' });
+  if (req.body.password !== undefined) {
+    if (!(req.body.password.length >= 8 && req.body.password.length <= 16)) {
+      return res.status(400).json({ msg: 'Password must be between 8 and 16 characters' });
+    }
   }
   return next();
 };
