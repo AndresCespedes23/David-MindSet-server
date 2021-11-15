1d8c80cff54690c1ecfd7e83c3c4aad9642fff5b
Sofía Pagura <sofiapagura@gmail.com>
2021-11-15 19:10:06 -0300

    Take back the others files

---
+++ controllers/psychologists.js
@@ -0,0 +1,34 @@
+/* const psyList = require("../data/psychologists"); */
+
+const getAll = (req, res) => {
+  res.status(501).json({ msg: 'Not implemented' });
+};
+
+const getById = (req, res) => {
+  res.status(501).json({ msg: 'Not implemented' });
+};
+
+const getByName = (req, res) => {
+  res.status(501).json({ msg: 'Not implemented' });
+};
+
+const add = (req, res) => {
+  res.status(501).json({ msg: 'Not implemented' });
+};
+
+const edit = (req, res) => {
+  res.status(501).json({ msg: 'Not implemented' });
+};
+
+const remove = (req, res) => {
+  res.status(501).json({ msg: 'Not implemented' });
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getByName,
+  add,
+  edit,
+  remove,
+};
