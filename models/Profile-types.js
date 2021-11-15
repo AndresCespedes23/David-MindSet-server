384e8c7ecf63fde8de5d0187741fd7f638a9c7ac
Pablo Balbo <pablobalbo6@gmail.com>
2021-11-15 11:39:04 -0300

    Max length added

--- models/Profile-types.js
+++ models/Profile-types.js
@@ -4,8 +4,16 @@ const { Schema, model } = mongoose;
 
 const profileTypesSchema = new Schema({
   id: Schema.Types.ObjectId,
-  name: { type: String, minlength: 1, required: true },
-  isActive: { type: Boolean, default: true },
+  name: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 module.exports = model('ProfileTypes', profileTypesSchema);
