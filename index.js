14670eb20b6c7df1907e2c02206c0ea21ded324a
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-15 17:06:16 -0300

    deleted old commented routes

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 const port = 8000;
 
 /* MongoDB Atlas Connection */
-// 'mongodb+srv://userDavid:pswDavid@radiumcluster.ffqld.mongodb.net/mindset2021?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://radium:radium123456@cluster0.urj6t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (err) => {
+mongoose.connect('mongodb+srv://userDavid:pswDavid@radiumcluster.ffqld.mongodb.net/mindset2021?retryWrites=true&w=majority', (err) => {
   if (err) {
     console.log('error connecting database');
   } else {
@@ -21,18 +20,6 @@ app.use(cors());
 
 app.use('/api', routes);
 
-/* Copiar segun el recurso, los métodos y cambiar los get por lo que corresponda en cada route
-Para probar los métodos que no son get, se debe usar Postman
-Usar el ejemplo de psychologist */
-
-/*
-app.get('/interviews', interviews.getAll);
-app.get('/interviews/add', interviews.add);
-app.get('/interviews/:id', interviews.getById);
-app.get('/interviews/byCompany/:idCompany', interviews.getByCompany);
-app.get('/interviews/edit/:id', interviews.edit);
-app.get('/interviews/remove/:id', interviews.remove);
-*/
 app.listen(port, () => {
   console.log(`Open your browser in http://localhost:${port}`);
 });
