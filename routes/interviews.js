a5b0b33ffe920b1be7584172e9d3e72cf923ca2a
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-15 12:07:12 -0300

    organize the routes

--- routes/interviews.js
+++ routes/interviews.js
@@ -6,10 +6,10 @@ const interviews = require('../controllers/interviews');
 const { validateFormat, isNotEmpty } = require('../validators/interviews');
 
 router.get('/', interviews.getAll);
+router.get('/search', validateFormat, interviews.search);
+router.get('/:id', validateFormat, interviews.getById);
 router.post('/', validateFormat, isNotEmpty, interviews.add);
 router.put('/:id', validateFormat, interviews.edit);
 router.delete('/:id', validateFormat, interviews.remove);
-router.get('/search', validateFormat, interviews.search);
-router.get('/:id', validateFormat, interviews.getById);
 
 module.exports = router;
