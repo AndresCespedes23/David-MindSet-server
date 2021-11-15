fc45cb4f77c0df18c60d350443e81d8732db8bfb
Sofía Pagura <sofiapagura@gmail.com>
2021-11-14 21:25:50 -0300

    Create validators

--- routes/administrators.js
+++ routes/administrators.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const administrators = require('../controllers/administrators');
-// tmb a la carpeta de funciones
+const { isNotEmpty, validateFormat, validateLength } = require('../validators/administrators');
 
 router.get('/', administrators.getAll);
 router.get('/search', administrators.search);
 router.get('/:id', administrators.getById);
-router.post('/', administrators.add);
-router.put('/:id', administrators.edit);
+router.post('/', isNotEmpty, validateFormat, validateLength, administrators.add);
+router.put('/:id', validateFormat, validateLength, administrators.edit);
 router.delete('/:id', administrators.remove);
 
 module.exports = router;
--- validators/administrators.js
+++ validators/administrators.js
@@ -1,21 +1,57 @@
 const mongoose = require('mongoose');
 
-const isNotEmpy = (req, res, next) => {
+const isNotEmpty = (req, res, next) => {
   if (!req.body.firstName) {
-    return res.status(400).json({ msg: 'First name is required'});
+    return res.status(400).json({ msg: 'First name is required' });
   }
   if (!req.body.lastName) {
-    return res.status(400).json({ msg: 'First name is required'});
+    return res.status(400).json({ msg: 'Last name is required' });
   }
   if (!req.body.email) {
-    return res.status(400).json({ msg: 'First name is required'});
+    return res.status(400).json({ msg: 'Email is required' });
   }
   if (!req.body.password) {
-    return res.status(400).json({ msg: 'First name is required'});
+    return res.status(400).json({ msg: 'Password is required' });
+  }
+  return next();
+};
+
+const validateFormat = (req, res, next) => {
+  if (req.body.firstName && !mongoose.Types.ObjectId.isValid(req.body.firstName)) {
+    return res.status(400).json({ msg: 'Invalid first name' });
+  }
+  if (req.body.lastName && !mongoose.Types.ObjectId.isValid(req.body.lastName)) {
+    return res.status(400).json({ msg: 'Invalid last name' });
+  }
+  if (req.body.email && !mongoose.Types.ObjectId.isValid(req.body.email)
+    && req.body.email.includes('@', 2) && !req.body.email.includes('@', -1)
+    && req.body.email.includes('.', 5) && !req.body.email.includes('.', -1)) {
+    return res.status(400).json({ msg: 'Invalid email' });
+  }
+  if (req.body.password && !mongoose.Types.ObjectId.isValid(req.body.password)) {
+    return res.status(400).json({ msg: 'Invalid password' });
+  }
+  return next();
+};
+
+const validateLength = (req, res, next) => {
+  if (req.body.firstName && req.body.firstName.length >= 2 && req.body.firstName.length <= 40) {
+    return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
+  }
+  if (req.body.lastName && req.body.lastName.length >= 2 && req.body.lastName.length <= 40) {
+    return res.status(400).json({ msg: 'Last name should be beetween 2 and 40 characters' });
+  }
+  if (req.body.email && req.body.email.length >= 5 && req.body.email.length <= 40) {
+    return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
+  }
+  if (req.body.password && req.body.password.length >= 8 && req.body.password.length <= 16) {
+    return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
   }
   return next();
 };
 
 module.exports = {
-    isNotEmpty,
+  isNotEmpty,
+  validateFormat,
+  validateLength,
 };
