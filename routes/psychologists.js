90ee2252473a8becf37278d682b49a8cee711c38
Valetín Feraudo <valentinferaudo@gmail.com>
2021-11-15 16:44:37 -0300

    Update endpoints

--- routes/psychologists.js
+++ routes/psychologists.js
@@ -7,8 +7,8 @@ const { isNotEmpty, validateLength, validateFormat } = require('../validators/ps
 router.get('/', psychologists.getAll);
 router.get('/search', psychologists.search);
 router.get('/:id', validateFormat, psychologists.getById);
-router.post('/add', isNotEmpty, validateFormat, validateLength, psychologists.add);
-router.put('/edit/:id', validateFormat, validateLength, psychologists.edit);
-router.delete('/remove/:id', validateFormat, psychologists.remove);
+router.post('/', isNotEmpty, validateFormat, validateLength, psychologists.add);
+router.put('/:id', validateFormat, validateLength, psychologists.edit);
+router.delete('/:id', validateFormat, psychologists.remove);
 
 module.exports = router;
