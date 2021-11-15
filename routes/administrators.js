e19d451ce5e1df9ddbdee2e0118d8a3b82a0bc00
Sofía Pagura <sofiapagura@gmail.com>
2021-11-14 22:20:01 -0300

    Fix validations

--- routes/administrators.js
+++ routes/administrators.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 const administrators = require('../controllers/administrators');
-const { isNotEmpty, validateId, validateEmail, validateLength } = require('../validators/administrators');
+const {
+  isNotEmpty, validateId, validateEmail, validateLength,
+} = require('../validators/administrators');
 
 router.get('/', administrators.getAll);
 router.get('/search', validateId, administrators.search);
 router.get('/:id', validateId, administrators.getById);
 router.post('/', isNotEmpty, validateId, validateEmail, validateLength, administrators.add);
-router.put('/:id', validateId, validateLength, administrators.edit);
+router.put('/:id', validateId, administrators.edit);
 router.delete('/:id', validateId, administrators.remove);
 
 module.exports = router;
--- validators/administrators.js
+++ validators/administrators.js
@@ -33,14 +33,12 @@ const validateId = (req, res, next) => {
 
 const validateEmail = (req, res, next) => {
   if (req.body.email
-    && req.body.email.includes('@', 2)
-    && !req.body.email.includes('@', -1)
-    && req.body.email.includes('.', 5)
-    && !req.body.email.includes('.', -1)) {
+    && req.body.email.includes('@')
+    && req.body.email.includes('.')) {
     return res.status(400).json({ msg: 'Invalid email' });
   }
   return next();
-  };
+};
 
 const validateLength = (req, res, next) => {
   if ((req.body.firstName
@@ -48,24 +46,23 @@ const validateLength = (req, res, next) => {
     || req.body.firstName.length >= 40) {
     return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
   }
-  if (req.body.lastName
-    && req.body.lastName.length <= 2
-    && req.body.lastName.length >= 40) {
+  if ((req.body.lastName
+    && req.body.lastName.length <= 2)
+    || req.body.lastName.length >= 40) {
     return res.status(400).json({ msg: 'Last name should be beetween 2 and 40 characters' });
   }
-  if (req.body.email
-    && req.body.email.length <= 5
-    && req.body.email.length >= 40) {
+  if ((req.body.email
+    && req.body.email.length <= 5)
+    || req.body.email.length >= 40) {
     return res.status(400).json({ msg: 'Email should be beetween 5 and 40 characters' });
   }
-  if (req.body.password
-    && req.body.password.length <= 8
-    && req.body.password.length >= 16) {
+  if ((req.body.password
+    && req.body.password.length <= 8)
+    || req.body.password.length >= 16) {
     return res.status(400).json({ msg: 'Password should be beetween 8 and 16 characters' });
   }
   return next();
 };
-
 module.exports = {
   isNotEmpty,
   validateId,
