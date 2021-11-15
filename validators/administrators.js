fa43ff6cd231b1b040435eb0d32af5b2c88e51a4
Sofía Pagura <sofiapagura@gmail.com>
2021-11-14 21:44:02 -0300

    Fix sintaxis

--- validators/administrators.js
+++ validators/administrators.js
@@ -17,35 +17,36 @@ const isNotEmpty = (req, res, next) => {
 };
 
 const validateFormat = (req, res, next) => {
-  if (req.body.firstName && !mongoose.Types.ObjectId.isValid(req.body.firstName)) {
-    return res.status(400).json({ msg: 'Invalid first name' });
-  }
-  if (req.body.lastName && !mongoose.Types.ObjectId.isValid(req.body.lastName)) {
-    return res.status(400).json({ msg: 'Invalid last name' });
-  }
-  if (req.body.email && !mongoose.Types.ObjectId.isValid(req.body.email)
-    && req.body.email.includes('@', 2) && !req.body.email.includes('@', -1)
-    && req.body.email.includes('.', 5) && !req.body.email.includes('.', -1)) {
+  if (req.body.email
+    && req.body.email.includes('@', 2)
+    && !req.body.email.includes('@', -1)
+    && req.body.email.includes('.', 5)
+    && !req.body.email.includes('.', -1)) {
     return res.status(400).json({ msg: 'Invalid email' });
-  }
-  if (req.body.password && !mongoose.Types.ObjectId.isValid(req.body.password)) {
-    return res.status(400).json({ msg: 'Invalid password' });
   }
   return next();
 };
 
 const validateLength = (req, res, next) => {
-  if (req.body.firstName && req.body.firstName.length >= 2 && req.body.firstName.length <= 40) {
+  if (req.body.firstName
+    && req.body.firstName.length >= 2
+    && req.body.firstName.length <= 40) {
     return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
   }
-  if (req.body.lastName && req.body.lastName.length >= 2 && req.body.lastName.length <= 40) {
+  if (req.body.lastName
+    && req.body.lastName.length >= 2
+    && req.body.lastName.length <= 40) {
     return res.status(400).json({ msg: 'Last name should be beetween 2 and 40 characters' });
   }
-  if (req.body.email && req.body.email.length >= 5 && req.body.email.length <= 40) {
-    return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
+  if (req.body.email
+    && req.body.email.length >= 5
+    && req.body.email.length <= 40) {
+    return res.status(400).json({ msg: 'First name should be beetween 5 and 40 characters' });
   }
-  if (req.body.password && req.body.password.length >= 8 && req.body.password.length <= 16) {
-    return res.status(400).json({ msg: 'First name should be beetween 2 and 40 characters' });
+  if (req.body.password
+    && req.body.password.length >= 8
+    && req.body.password.length <= 16) {
+    return res.status(400).json({ msg: 'First name should be beetween 8 and 16 characters' });
   }
   return next();
 };
