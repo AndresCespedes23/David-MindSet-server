e385ccf08a5be320e1ba78a5e960398138941e11
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-20 16:35:35 -0300

    MS-85 get psychologists function done

---
+++ public/js/psychologists/listPsychologists.js
@@ -0,0 +1,116 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-underscore-dangle */
+const deleteButtons = document.querySelectorAll('.delete-button');
+const cancelButton = document.getElementById('cancel-button');
+const modal = document.getElementById('modal');
+const confirmDeleteButton = document.getElementById('confirm-delete-button');
+const tableContent = document.getElementById('table-content');
+
+deleteButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    modal.classList.toggle('hide');
+  });
+});
+
+cancelButton.addEventListener('click', () => {
+  modal.classList.toggle('hide');
+});
+
+const deletePsychologist = (psychologistId) => {
+  fetch(`https://basd-2021-david-mindset-dev.herokuapp.com/api/psychologists/${psychologistId}`, {
+    method: 'DELETE',
+  })
+    .then((response) => response.json())
+    .then(() => {
+      modal.classList.add('hide');
+      while (tableContent.hasChildNodes()) {
+        tableContent.removeChild(tableContent.firstChild);
+      }
+      getPsychologists();
+      console.log();
+    })
+    .catch((error) => console.log(error));
+};
+
+const openDeleteModal = (psychologists) => {
+  console.log(psychologists);
+  const dataModal = document.getElementById('data-modal');
+  dataModal.textContent = `First Name: ${psychologists.firstName}, Last Name: ${psychologists.lastName}, Email: ${psychologists.email}, Password: ${psychologists.password}, Picture: ${psychologists.pictureUrl}, Turns: ${psychologists.turns}`;
+  modal.classList.remove('hide');
+  confirmDeleteButton.onclick = () => deletePsychologist(psychologists._id);
+};
+
+const createDeleteButton = (psychologists) => {
+  const buttonDelete = document.createElement('button');
+  buttonDelete.setAttribute('class', 'delete-button');
+  const deleteLogo = document.createElement('span');
+  deleteLogo.classList.add('material-icons-outlined');
+  deleteLogo.textContent = 'clear';
+  buttonDelete.innerHTML = deleteLogo.outerHTML;
+  buttonDelete.addEventListener('click', () => {
+    openDeleteModal(psychologists);
+  });
+  return buttonDelete;
+};
+
+const openUpdatePsychologist = (psychologist) => {
+  window.location.href = `${window.location.origin}/mindset-2021/public/views/psychologists/formPsychologists.html?_id=${psychologist._id}`;
+};
+
+const createUpdateButton = (psychologist) => {
+  const buttonUpdate = document.createElement('button');
+  const updateLogo = document.createElement('span');
+  updateLogo.classList.add('material-icons-outlined');
+  updateLogo.textContent = 'edit';
+  buttonUpdate.innerHTML = updateLogo.outerHTML;
+  buttonUpdate.addEventListener('click', () => {
+    openUpdatePsychologist(psychologist);
+  });
+  return buttonUpdate;
+};
+
+const getPsychologists = () => {
+  fetch('https://basd-2021-david-mindset-dev.herokuapp.com/api/psychologists')
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.length === 0) {
+        const noPsychologists = document.createElement('h2');
+        noPsychologists.innerText = 'No psychologists found';
+        tableContent.appendChild(noPsychologists);
+      } else {
+        response.psychologists.forEach((psychologist) => {
+          const tr = document.createElement('tr');
+          const tdFirstName = document.createElement('td');
+          const tdLastName = document.createElement('td');
+          const tdEmail = document.createElement('td');
+          const tdPassword = document.createElement('td');
+          const tdPictureUrl = document.createElement('td');
+          const tdTurns = document.createElement('td');
+          const deleteButton = createDeleteButton(psychologist);
+          const updateButton = createUpdateButton(psychologist);
+          tdFirstName.innerText = psychologist.firstName;
+          tdLastName.innerText = psychologist.lastName;
+          tdEmail.innerText = psychologist.email;
+          tdPassword.innerText = psychologist.password;
+          tdPictureUrl.innerText = psychologist.tdPictureUrl;
+          tdTurns.innerText = psychologist.turns;
+          tr.append(
+            tdFirstName,
+            tdLastName,
+            tdEmail,
+            tdPassword,
+            tdPictureUrl,
+            tdTurns,
+            deleteButton,
+            updateButton,
+          );
+          tableContent.append(tr);
+        });
+      }
+    })
+    .catch((error) => console.log(error));
+};
+
+window.onload = () => {
+  getPsychologists();
+};
