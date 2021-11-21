6203b9c4168709466454c9c5572f0abcae85e2e7
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2021-11-21 10:55:37 -0300

    CRUD functions

--- public/js/companies/profile-types/formProfileTypes.js
+++ public/js/companies/profile-types/formProfileTypes.js
@@ -1,54 +1,35 @@
-const psychologistSelect = document.getElementById('psychologist');
-const candidateSelect = document.getElementById('candidate');
-const dateInput = document.getElementById('date');
-const dateErrorMsg = document.getElementById('date-error-msg');
+const nameProfile = document.getElementById('name');
+let nameProfileValue = nameProfile.value;
+const nameProfileError = document.getElementById('name-error');
 const saveButton = document.getElementById('button-green');
 const modalOk = document.getElementById('modal-ok');
 const modalOkConfirm = document.getElementById('modal-ok-confirm');
 const params = new URLSearchParams(window.location.search);
 let errorList = [];
-let psychologistsArray = [];
-let candidatesArray = [];
 
 modalOkConfirm.addEventListener('click', () => {
   modalOk.classList.toggle('hide');
-  window.location.href = `${window.location.origin}/api/views/sessions/listProfileTypes.html`;
+  window.location.href = `${window.location.origin}/api/views/psychologists/listPsychologists.html`;
 });
 
-const getProfiles = () => {
-  fetch('https://basd-2021-david-mindset-dev.herokuapp.com/api/profile-types')
-    .then((response) => response.json())
-    .then((response) => {
-      profilesArray = response.profiles;
-      response.forEach((profiles) => {
-        const option = document.createElement('option');
-        option.innerText = `${profiles.name}`;
-        option.value = profiles._id;
-        psychologistSelect.append(option);
-      });
-    });
-};
-
+// Modal that shows success message
 const openOkModal = (response) => {
   modalOk.classList.remove('hide');
   const modalOkTitle = document.getElementById('modal-ok-title');
   modalOkTitle.textContent = response.msg;
   const modalOkData = document.getElementById('modal-ok-data');
-  modalOkData.textContent = `Psychologist: ${response.name}.`;
+  modalOkData.textContent = `Name: ${response.name}.`;
 };
 
-const addProfile = (data) => {
-  fetch(
-    'https://basd-2021-david-mindset-dev.herokuapp.com/api/profile-types',
-    {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
+const addProfile = (profile) => {
+  fetch('https://basd-2021-david-mindset-dev.herokuapp.com/api/profile-types', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify(profile),
+  })
     .then((response) => response.json())
     .then((response) => {
       openOkModal(response);
@@ -58,18 +39,15 @@ const addProfile = (data) => {
     });
 };
 
-const updateProfile = () => {
-  fetch(
-    `https://basd-2021-david-mindset-dev.herokuapp.com/api/profile-types/${params.get('_id')}`,
-    {
-      method: 'PUT',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
+const updateProfile = (profile) => {
+  fetch(`https://basd-2021-david-mindset-dev.herokuapp.com/api/profile-types/${params.get('_id')}`, {
+    method: 'PUT',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify(profile),
+  })
     .then((response) => response.json())
     .then((response) => {
       openOkModal(response);
@@ -79,55 +57,49 @@ const updateProfile = () => {
     });
 };
 
+// Function that creates an object with the formulary data and decides between add or edit
 const saveProfile = () => {
-  const data = {
-    name: psychologistSelect.value,
+  const profileTypes = {
+    name: nameProfileValue,
   };
   if (params.get('_id')) {
-    updateProfile(data);
+    updateProfile(profileTypes);
   } else {
-    addProfile(data);
+    addProfile(profileTypes);
   }
 };
 
-const isNotEmpty = () => {
-  if (psychologistSelect.value === '') {
-    errorList.push('Psychologist is required');
-  }
-  if (candidateSelect.value === '') {
-    errorList.push('Candidate is required');
-  }
-  if (dateInput.value === '') {
-    errorList.push('Date is required');
-    dateInput.classList.add('input-error');
-    dateErrorMsg.classList.remove('hide');
-  } else {
-    dateInput.classList.remove('input-error');
-    dateErrorMsg.classList.add('hide');
+// Validations
+const validateLength = () => {
+  if (nameProfileValue !== undefined) {
+    if (!(nameProfileValue.length >= 2 && nameProfileValue.length <= 30)) {
+      errorList.push('Name must be between 2 and 30 characters');
+      nameProfile.classList.add('input-error');
+      nameProfileError.classList.remove('hide');
+      nameProfileError.textContent = '*Name must be between 2 and 30 characters.';
+    }
   }
 };
 
-const getPsychologistName = (id) => {
-  const psychologist = psychologistsArray.find((element) => element._id === id);
-  return `${psychologist.firstName} ${psychologist.lastName}`;
+const specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
+
+const validateFormat = () => {
+  if (nameProfileValue !== undefined) {
+    if (nameProfileValue.includes(specialChars)) {
+      errorList.push('Only letters, please');
+      nameProfile.classList.add('input-error');
+      nameProfileError.classList.remove('hide');
+      nameProfileError.textContent = '*Only letters, please.';
+    }
+  }
 };
 
-const getCandidateName = (id) => {
-  const candidate = candidatesArray.find((element) => element._id === id);
-  return `${candidate.firstName} ${candidate.lastName}`;
-};
-
-const getSession = () => {
-  fetch(`https://basd-2021-david-mindset-dev.herokuapp.com/api/sessions/${params.get('_id')}`)
+// Search psychologist so that when I edit, it shows me the data of the psychologist
+const getProfile = () => {
+  fetch(`https://basd-2021-david-mindset-dev.herokuapp.com/api/profile-types/${params.get('_id')}`)
     .then((response) => response.json())
     .then((response) => {
-      psychologistSelect.value = response.data.idPsychologists;
-      // Missing Populate
-      psychologistSelect.text = getPsychologistName(response.data.idPsychologists);
-      candidateSelect.value = response.data.idCandidate;
-      // Missing Populate
-      candidateSelect.text = getCandidateName(response.data.idCandidate);
-      dateInput.value = response.data.date.split('T')[0];
+      nameProfileValue = response.name;
     })
     .catch((err) => {
       console.log(err);
@@ -135,20 +107,20 @@ const getSession = () => {
 };
 
 window.onload = () => {
-  getPsychologists();
-  getCandidates();
   if (params.get('_id')) {
+    getProfile();
     const title = document.getElementById('title');
-    title.innerText = 'Edit Session';
+    title.innerText = 'Edit Profile';
     saveButton.value = 'UPDATE';
-    getSession();
   }
 };
 
+// If there are no errors, save the psychologist
 saveButton.addEventListener('click', () => {
   errorList = [];
-  isNotEmpty();
+  validateLength();
+  validateFormat();
   if (errorList.length === 0) {
-    return saveSession();
+    saveProfile();
   }
 });
