6e8243468b32b70fbaef6c9f4882ea00f1131c7f
Pablo Balbo <pablobalbo6@gmail.com>
2021-11-21 02:37:24 -0300

    MS-113: Add delete function

--- public/js/admin/listAdmin.js
+++ public/js/admin/listAdmin.js
@@ -1,97 +1,117 @@
-const deleteButtons = document.querySelectorAll('.delete-button'),
-      cancelButton = document.getElementById('cancel-button'),
-      modal = document.getElementById('modal'),
-      confirmDeleteButton = document.getElementById('confirm-delete-button'),
-      tableContent = document.getElementById('table-content'),
-      dataModal = document.getElementById('data-modal');
+const deleteButtons = document.querySelectorAll(".delete-button");
+const cancelButton = document.getElementById("cancel-button");
+const modal = document.getElementById("modal");
+const confirmDeleteButton = document.getElementById("confirm-delete-button");
+const tableContent = document.getElementById("table-content");
 
 deleteButtons.forEach((button) => {
-    button.addEventListener('click', () => modal.classList.toggle('hide'));
+  button.addEventListener("click", () => modal.classList.toggle("hide"));
 });
 
-cancelButton.addEventListener('click', () => modal.classList.toggle('hide'));
+// Event to close the modal to confirm remove
+cancelButton.addEventListener("click", () => modal.classList.toggle("hide"));
 
-const getSessions = () => {
-    fetch('https://basd-2021-david-mindset-dev.herokuapp.com/api/administrator')
-    .then((response) => response.json())
-    .then((response) => {
-        const tableSession = document.getElementById('table-session');
-        if (response.data.length === 0) {
-            tableSession.classList.add('hide');
-            const emptyTableMsg = document.getElementById('empty-table-msg');
-            emptyTableMsg.classList.remove('hide');
-        } else {
-            tableSession.classList.remove('hide');
-            response.data.forEach((administrator) => {
-                const tr = document.createElement('tr');
-                const tdCompany = document.createElement('td');
-                const tdCandidate = document.createElement('td');
-                const tdDate = document.createElement('td');
-                const deleteIcon = createDeleteButton(administrator);
-                const updateIcon = createUpdateButton(administrator);
-                tdCompany.innerText = interviews.idCompany;
-                tdCandidate.innerText = interviews.idCandidate;
-                tdDate.innerText = interviews.date;
-                tr.append(tdCompany, tdCandidate, tdDate, deleteIcon, updateIcon);
-                tableContent.append(tr);
-          });
-        }
-      });
-  };
-
-const deleteSession = (adminId) => {
-    fetch(`https://basd-2021-david-mindset-dev.herokuapp.com/api/administrator/${adminId}`, {
-        method: 'DELETE',
-    })
+const deleteAdmin = (adminId) => {
+  fetch(
+    `https://basd-2021-david-mindset-dev.herokuapp.com/api/administrators/${adminId}`,
+    {
+      method: "DELETE",
+    }
+  )
     .then((response) => response.json())
     .then(() => {
-        modal.classList.add('hide');
-        // Set table empty
-        while (tableContent.hasChildNodes()) {
+      modal.classList.add("hide");
+      while (tableContent.hasChildNodes()) {
         tableContent.removeChild(tableContent.firstChild);
-    }
-    getSessions();
+      }
+      getAdmins();
     })
-    .catch((err) => {
-    console.log(err);
-    });
+    .catch((error) => console.log(error));
 };
 
-let openDeleteModal = (interview) => {
-    dataModal.textContent = `Adminstrator: ${administrator.idNumber}`;
-    modal.classList.remove('hide');
-    confirmDeleteButton.onclick = () => deleteSession(administrator._id);
+// Modal to confirm remove
+const openDeleteModal = (psychologists) => {
+  const dataModal = document.getElementById("data-modal");
+  dataModal.textContent = `First Name: ${psychologists.firstName}, Last Name: ${psychologists.lastName}, Email: ${psychologists.email}, Password: ${psychologists.password}, Picture: ${psychologists.pictureUrl}, Turns: ${psychologists.turns}`;
+  modal.classList.remove("hide");
+  confirmDeleteButton.onclick = () => deletePsychologist(psychologists._id);
 };
 
-const createDeleteButton = (interview) => {
-    const buttonDelete = document.createElement('button');
-    buttonDelete.setAttribute('class', 'delete-button');
-    const deleteLogo = document.createElement('span');
-    deleteLogo.classList.add('material-icons-outlined');
-    deleteLogo.textContent = 'clear';
-    buttonDelete.innerHTML = deleteLogo.outerHTML;
-    buttonDelete.addEventListener('click', () => {
-        openDeleteModal(interview);
-    });
-    return buttonDelete;
+// Function that creates the remove button of each table row
+const createDeleteButton = (psychologists) => {
+  const buttonDelete = document.createElement("button");
+  buttonDelete.setAttribute("class", "delete-button");
+  const deleteLogo = document.createElement("span");
+  deleteLogo.classList.add("material-icons-outlined");
+  deleteLogo.textContent = "clear";
+  buttonDelete.innerHTML = deleteLogo.outerHTML;
+  buttonDelete.addEventListener("click", () => {
+    openDeleteModal(psychologists);
+  });
+  return buttonDelete;
 };
 
-const openUpdateSession = (interview) => {
-  window.location.href = `${window.location.origin}/public/views/interviews/formInterviews.html?_id=${interview._id}`;
+// Function to go into the edit form
+const openUpdatePsychologist = (psychologist) => {
+  window.location.href = `${window.location.origin}/api/views/psychologists/formPsychologists.html?_id=${psychologist._id}`;
 };
 
-const createUpdateButton = (interview) => {
-    const buttonUpdate = document.createElement('button');
-    const updateLogo = document.createElement('span');
-    updateLogo.classList.add('material-icons-outlined');
-    updateLogo.textContent = 'edit';
-    buttonUpdate.innerHTML = updateLogo.outerHTML;
-    buttonUpdate.addEventListener('click', () => {
-        openUpdateSession(interview);
-    });
-    return buttonUpdate;
+// Function that creates the edit button of each table row
+const createUpdateButton = (psychologist) => {
+  const buttonUpdate = document.createElement("button");
+  const updateLogo = document.createElement("span");
+  updateLogo.classList.add("material-icons-outlined");
+  updateLogo.textContent = "edit";
+  buttonUpdate.innerHTML = updateLogo.outerHTML;
+  buttonUpdate.addEventListener("click", () => {
+    openUpdatePsychologist(psychologist);
+  });
+  return buttonUpdate;
+};
+
+// Function that obtains the psychologists and then fill the table
+const getAdmins = () => {
+  fetch("https://basd-2021-david-mindset-dev.herokuapp.com/api/psychologists")
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.length === 0) {
+        const noPsychologists = document.createElement("h2");
+        noPsychologists.innerText = "No psychologists found";
+        tableContent.appendChild(noPsychologists);
+      } else {
+        response.psychologists.forEach((psychologist) => {
+          const tr = document.createElement("tr");
+          const tdFirstName = document.createElement("td");
+          const tdLastName = document.createElement("td");
+          const tdEmail = document.createElement("td");
+          const tdPassword = document.createElement("td");
+          const tdPictureUrl = document.createElement("td");
+          const tdTurns = document.createElement("td");
+          const deleteButton = createDeleteButton(psychologist);
+          const updateButton = createUpdateButton(psychologist);
+          tdFirstName.innerText = psychologist.firstName;
+          tdLastName.innerText = psychologist.lastName;
+          tdEmail.innerText = psychologist.email;
+          tdPassword.innerText = psychologist.password;
+          tdPictureUrl.innerText = psychologist.tdPictureUrl;
+          tdTurns.innerText = psychologist.turns;
+          tr.append(
+            tdFirstName,
+            tdLastName,
+            tdEmail,
+            tdPassword,
+            tdPictureUrl,
+            tdTurns,
+            deleteButton,
+            updateButton
+          );
+          tableContent.append(tr);
+        });
+      }
+    })
+    .catch((error) => console.log(error));
 };
 
 window.onload = () => {
-  getSessions();
+  getAdmins();
 };
