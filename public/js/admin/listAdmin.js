de7db07669b2f4c0bec160e10f170f33fce424ea
Pablo Balbo <pablobalbo6@gmail.com>
2021-11-21 02:50:58 -0300

    MS-88: Get function added

--- public/js/admin/listAdmin.js
+++ public/js/admin/listAdmin.js
@@ -30,15 +30,15 @@ const deleteAdmin = (adminId) => {
 };
 
 // Modal to confirm remove
-const openDeleteModal = (psychologists) => {
+const openDeleteModal = (administrators) => {
   const dataModal = document.getElementById("data-modal");
-  dataModal.textContent = `First Name: ${psychologists.firstName}, Last Name: ${psychologists.lastName}, Email: ${psychologists.email}, Password: ${psychologists.password}, Picture: ${psychologists.pictureUrl}, Turns: ${psychologists.turns}`;
+  dataModal.textContent = `First Name: ${administrators.firstName}, Last Name: ${administrators.lastName}, Email: ${administrators.email}, Password: ${administrators.password}`;
   modal.classList.remove("hide");
-  confirmDeleteButton.onclick = () => deletePsychologist(psychologists._id);
+  confirmDeleteButton.onclick = () => deleteAdmin(administrators._id);
 };
 
 // Function that creates the remove button of each table row
-const createDeleteButton = (psychologists) => {
+const createDeleteButton = (administrators) => {
   const buttonDelete = document.createElement("button");
   buttonDelete.setAttribute("class", "delete-button");
   const deleteLogo = document.createElement("span");
@@ -46,62 +46,56 @@ const createDeleteButton = (psychologists) => {
   deleteLogo.textContent = "clear";
   buttonDelete.innerHTML = deleteLogo.outerHTML;
   buttonDelete.addEventListener("click", () => {
-    openDeleteModal(psychologists);
+    openDeleteModal(administrators);
   });
   return buttonDelete;
 };
 
 // Function to go into the edit form
-const openUpdatePsychologist = (psychologist) => {
-  window.location.href = `${window.location.origin}/api/views/psychologists/formPsychologists.html?_id=${psychologist._id}`;
+const openUpdateAdmin = (administrator) => {
+  window.location.href = `${window.location.origin}/api/views/admin/formAdmin.html?_id=${administrator._id}`;
 };
 
 // Function that creates the edit button of each table row
-const createUpdateButton = (psychologist) => {
+const createUpdateButton = (administrator) => {
   const buttonUpdate = document.createElement("button");
   const updateLogo = document.createElement("span");
   updateLogo.classList.add("material-icons-outlined");
   updateLogo.textContent = "edit";
   buttonUpdate.innerHTML = updateLogo.outerHTML;
   buttonUpdate.addEventListener("click", () => {
-    openUpdatePsychologist(psychologist);
+    openUpdateAdmin(administrator);
   });
   return buttonUpdate;
 };
 
-// Function that obtains the psychologists and then fill the table
+// Function that obtains the administrators and then fill the table
 const getAdmins = () => {
-  fetch("https://basd-2021-david-mindset-dev.herokuapp.com/api/psychologists")
+  fetch("https://basd-2021-david-mindset-dev.herokuapp.com/api/administrators")
     .then((response) => response.json())
     .then((response) => {
       if (response.length === 0) {
-        const noPsychologists = document.createElement("h2");
-        noPsychologists.innerText = "No psychologists found";
-        tableContent.appendChild(noPsychologists);
+        const noadministrators = document.createElement("h2");
+        noadministrators.innerText = "No administrators found";
+        tableContent.appendChild(noadministrators);
       } else {
-        response.psychologists.forEach((psychologist) => {
+        response.data.forEach((administrator) => {
           const tr = document.createElement("tr");
           const tdFirstName = document.createElement("td");
           const tdLastName = document.createElement("td");
           const tdEmail = document.createElement("td");
           const tdPassword = document.createElement("td");
-          const tdPictureUrl = document.createElement("td");
-          const tdTurns = document.createElement("td");
-          const deleteButton = createDeleteButton(psychologist);
-          const updateButton = createUpdateButton(psychologist);
-          tdFirstName.innerText = psychologist.firstName;
-          tdLastName.innerText = psychologist.lastName;
-          tdEmail.innerText = psychologist.email;
-          tdPassword.innerText = psychologist.password;
-          tdPictureUrl.innerText = psychologist.tdPictureUrl;
-          tdTurns.innerText = psychologist.turns;
+          const deleteButton = createDeleteButton(administrator);
+          const updateButton = createUpdateButton(administrator);
+          tdFirstName.innerText = administrator.firstName;
+          tdLastName.innerText = administrator.lastName;
+          tdEmail.innerText = administrator.email;
+          tdPassword.innerText = administrator.password;
           tr.append(
             tdFirstName,
             tdLastName,
             tdEmail,
             tdPassword,
-            tdPictureUrl,
-            tdTurns,
             deleteButton,
             updateButton
           );
