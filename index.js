f559cfd1fbd3ac20b22925a045cc8e2cd7efb582
Guido Glielmi <guidoglielmi@gmail.com>
2021-11-23 16:08:38 -0300

    comment erased

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-mongoose.connect(
-  'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false' /* process.env.DATABASE_URL */,
-  (err) => {
-    if (err) {
-      console.log('error connecting database');
-    } else {
-      console.log('database connected');
-    }
-  },
-);
+mongoose.connect(process.env.DATABASE_URL, (err) => {
+  if (err) {
+    console.log('error connecting database');
+  } else {
+    console.log('database connected');
+  }
+});
 
 app.use(express.json());
 
