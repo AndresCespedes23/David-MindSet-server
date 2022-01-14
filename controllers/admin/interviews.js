8b769cef02b25882e126ac19e6bb6d939be874b0
Eugenio Ponzone <eugenioponzone@gmail.com>
2022-01-14 18:11:15 -0300

    MS-91: refactor admin interviews

--- controllers/admin/interviews.js
+++ controllers/admin/interviews.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongoose').Types;
+
 const Interviews = require('../../models/Interviews');
 const { getCurrentWeekCandidate, getAvailableDatesCandidate } = require('../../helpers');
 
@@ -7,6 +9,7 @@ const getAll = (req, res) => {
   Interviews.find()
     .populate('idCompany', 'name')
     .populate('idCandidate', 'firstName lastName')
+    .populate('idOpenPosition', 'jobDescription startDate endDate')
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json({ msg: `Error: ${err}`, error: true }));
 };
@@ -16,6 +19,7 @@ const getById = (req, res) => {
   Interviews.findById(id)
     .populate('idCompany', 'name')
     .populate('idCandidate', 'firstName lastName')
+    .populate('idOpenPosition', 'jobDescription startDate endDate')
     .then((data) => {
       if (!data) return res.status(404).json({ msg: `${notFoundTxt} ID: ${id}`, error: true });
       return res.status(200).json(data);
@@ -38,28 +42,21 @@ const add = (req, res) => {
     .catch((err) => res.status(500).json({ msg: `Error: ${err}`, error: true }));
 };
 
-const edit = (req, res) => {
-  const { id } = req.params;
-  Interviews.findByIdAndUpdate(id, req.body, { new: true })
-    .populate('idCompany', 'name')
-    .populate('idCandidate', 'firstName lastName')
-    .then((data) => {
-      if (!data) return res.status(404).json({ msg: `${notFoundTxt} ID: ${id}`, error: true });
-      return res.status(200).json({ msg: 'Interview updated', data });
-    })
-    .catch((err) => res.status(500).json({ msg: `Error: ${err}`, error: true }));
-};
-
-const remove = (req, res) => {
-  const { id } = req.params;
-  Interviews.findByIdAndRemove(id)
-    .populate('idCompany', 'name')
-    .populate('idCandidate', 'firstName lastName')
-    .then((data) => {
-      if (!data) return res.status(404).json({ msg: `${notFoundTxt} ID: ${id}`, error: true });
-      return res.status(200).json({ msg: 'Interview deleted', data });
-    })
-    .catch((err) => res.status(500).json({ msg: `Error: ${err}`, error: true }));
+const edit = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const interview = await Interviews.findById({ _id: new ObjectId(id) });
+    if (!interview) return res.status(404).json({ msg: `${notFoundTxt} ID: ${id}`, error: true });
+    interview.status = req.body.status ? req.body.status : interview.status;
+    interview.result = req.body.result ? req.body.result : interview.result;
+    if (req.body.time || req.body.time) interview.status = 'pending';
+    interview.date = req.body.date ? req.body.date : interview.date;
+    interview.time = req.body.time ? req.body.time : interview.time;
+    const data = await Interviews.findByIdAndUpdate(id, interview, { new: true });
+    return res.status(200).json({ msg: 'Interview updated', data });
+  } catch (err) {
+    return res.status(500).json({ msg: `Error: ${err}`, error: true });
+  }
 };
 
 const getAvailableInterviews = async (req, res) => {
@@ -74,6 +71,5 @@ module.exports = {
   getById,
   add,
   edit,
-  remove,
   getAvailableInterviews,
 };
