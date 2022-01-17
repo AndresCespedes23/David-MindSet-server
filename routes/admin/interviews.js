3b2e2c5a09c68b83b755012f30de2ee5bfcb8fed
Baltazar Cortina <baltazarcortina@gmail.com>
2022-01-17 02:52:38 -0300

    MS-85: Add logic to delete outdated sessions and interviews when deleting a candidate or psychologist

--- routes/admin/interviews.js
+++ routes/admin/interviews.js
@@ -5,13 +5,25 @@ const router = express.Router();
 const interviews = require('../../controllers/admin/interviews');
 
 const {
-  validateFormat, isNotEmpty, interviewStillAvailable, interviewBetweenDates,
+  validateFormat,
+  isNotEmpty,
+  interviewStillAvailable,
+  interviewBetweenDates,
 } = require('../../validators/interviews');
 
 router.get('/', authMiddleware, interviews.getAll);
 router.get('/:id', authMiddleware, validateFormat, interviews.getById);
-router.post('/', authMiddleware, validateFormat, isNotEmpty, interviewStillAvailable, interviewBetweenDates, interviews.add);
+router.post(
+  '/',
+  authMiddleware,
+  validateFormat,
+  isNotEmpty,
+  interviewStillAvailable,
+  interviewBetweenDates,
+  interviews.add,
+);
 router.put('/:id', authMiddleware, validateFormat, interviews.edit);
 router.get('/availableDates/:id', authMiddleware, interviews.getAvailableInterviews);
+router.patch('/:id', validateFormat, interviews.cancelOutdatedInterviews);
 
 module.exports = router;
--- routes/admin/sessions.js
+++ routes/admin/sessions.js
@@ -3,17 +3,10 @@ const authMiddleware = require('../../middleware/authMiddleware');
 
 const router = express.Router();
 const sessions = require('../../controllers/admin/sessions');
-const {
-  validateFormat,
-  isNotEmpty,
-  validateLength,
-} = require('../../validators/sessions');
+const { validateFormat } = require('../../validators/sessions');
 
 router.get('/', sessions.getAll);
-router.post('/', isNotEmpty, validateLength, validateFormat, sessions.add);
-router.get('/search', authMiddleware, validateFormat, sessions.search);
-router.put('/:id', authMiddleware, validateFormat, sessions.edit);
-router.delete('/:id', authMiddleware, validateFormat, sessions.remove);
+router.patch('/:id', authMiddleware, validateFormat, sessions.cancelOutdatedSessions);
 router.get('/:id', authMiddleware, validateFormat, sessions.getById);
 
 module.exports = router;
